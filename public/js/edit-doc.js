f4ef3315b49f1a81fcf643db83052ee51f1eda98
Chris Birk <cmbirk@gmail.com>
2013-03-26 13:10:26 -0400

    add public files

--- public/js/edit-doc.js
+++ public/js/edit-doc.js
@@ -7,7 +7,6 @@
 
 function saveDocument(){
 	var doc_items = new Array();
-	var doc_id = $('#doc_id').val();
 	
 	$('.doc_item').each(function(){
 		var parent = $(this).parent('ol').parent('.doc_item');
@@ -26,11 +25,8 @@ function saveDocument(){
 		doc_items.push(ret);
 	});
 	
-	$.post('admin/admin-ajax.php', {"action":"save-doc", "doc_id": doc_id, "doc_items":doc_items}, function(data){
-		data = JSON.parse(data);
-		
-		$('#save_message').html(data.msg).removeClass('hidden');
-	});
+	var input = $('<input>').attr('type', 'hidden').attr('name', 'doc_items').val(JSON.stringify(doc_items));
+	$('#doc_content_form').append($(input));
 }
 
 $(document).ready(function(){
@@ -41,6 +37,9 @@ $(document).ready(function(){
 			toleranceElement: 'div',
 			maxLevels: 0
 		});
+		var input = $('<input>').attr('type', 'hidden').attr('name', 'deleted_ids').attr('id', 'deleted_ids').val(new Array());
+		$('#doc_content_form').append(input);
+		$('#doc_content_form').submit(saveDocument);
 		$('.doc_item .dropdown_arrow').click(dropdown_arrow_handler);
 		$('.delete_doc_item').click(delete_doc_handler);
 		$('.add_doc_item').click(add_doc_handler);
@@ -62,17 +61,18 @@ $(document).ready(function(){
 });
 
 function add_doc_handler(){
-	var doc_id = $('#doc_id').val();
+	var doc_id = $('input[name="doc_id"]').val();
 	var parent_id = $(this).siblings('input[name="content_id"]').val();
 	var success;
 	var new_id = -1;
 	var caller = $(this);
+	var token = $('input[name="csrf_token"]').val();
 	
-	//Get new content id
-	$.post('admin/admin-ajax.php', {'action':'add-doc-section', 'doc_id':doc_id, 'parent_id':parent_id}, function(data){
+	$.post('/dashboard/content', {'content': 'New Content', 'doc_id': doc_id, 'parent_id': parent_id, 'csrf_token': token}, function(data){
+		console.log(data);
 		data = JSON.parse(data);
 		success = data.success;
-		new_id = data.new_id;
+		new_id = data.id;
 		
 		if(success == false){
 			return;
@@ -80,51 +80,48 @@ function add_doc_handler(){
 		
 		//Create parent doc item
 		var doc_item = $('<li class="doc_item"></li>');
-		
+
 		//Create objects to be appended to parent doc item
-		var sib1 = $('<span></span>').append($('<img style="cursor:pointer;" src="/assets/i/arrow-down.png" alt="Dropdown Arrow" class="dropdown_arrow" />').click(dropdown_arrow_handler)).append(' New Content');
+		var sib1 = $('<span></span>').append($('<img style="cursor:pointer;" src="/img/arrow-down.png" alt="Dropdown Arrow" class="dropdown_arrow" />').click(dropdown_arrow_handler)).append(' New Content');
 		var sib2 = $('<input type="hidden" name="content_id" value="' + new_id + '" />');
 		var sib3 = $('<p class="add_doc_item">+</p>').click(add_doc_handler);
 		var sib4 = $('<p class="delete_doc_item">x</p>').click(delete_doc_handler);
 		var sib5 = $('<p class="doc_item_content expanded"><textarea>New Content</textarea></p>');
-		
+
 		//Append a div and the child elements to the parent doc item
 		doc_item.append($('<div class="sort_handle"></div>').append([sib1, sib2, sib3, sib4, sib5]));
-		
+
 		//Append the parent doc item to the list
 		var childList = caller.parent('div').parent('.doc_item').find('> ol');
-		
+
 		//Create the child list if there is none
 		if(childList.length == 0){
 			caller.parent('div').parent('.doc_item').find('.sort_handle').after('<ol></ol>');
 		}
-		
+
 		//Add the child
 		caller.parent('div').parent('.doc_item').find('> ol').prepend(doc_item);
+		
 	});
 }
 function delete_doc_handler(){
-	$('.delete_doc_item').addClass('hidden');
-	$('.floatingCirclesG').removeClass('hidden');
-	$('.f_circleG').addClass('animated');
+	var toRemove = $(this).parent('.sort_handle').parent('.doc_item');
+	var children = toRemove.find('.doc_item');
+	var deletedIds = new Array();
+
+	$(children).each(function(){
+		deletedIds.push($(this).children('.sort_handle').children('input[name="content_id"]').val());
+	});
+	deletedIds.push($(this).siblings('input[name="content_id"]').val());
 	
-	var success = true;
-	var id = $(this).siblings('input[name="content_id"]').val();
-	try{
-		$.post('admin/admin-ajax.php', {'action':'delete-doc-section', 'id':id}, function(data){
-			data = JSON.parse(data);
-			if(data.success == false){
-				alert(data.msg);
-			}
-			else{
-				window.location.reload();
-			}
-		});
+	if($('#deleted_ids').val().length > 0){
+		var prevIds = $('#deleted_ids').val().split();
+		$('#deleted_ids').val(_.union(prevIds, deletedIds));
+	}else{
+		$('#deleted_ids').val(deletedIds);
 	}
-	catch(err)
-	{
-		console.log(err);
-	}
+	
+	toRemove.remove();
 }
 function dropdown_arrow_handler(){
 	var sibling_content = $(this).parent('span').siblings('.doc_item_content');
--- public/js/edit-nav.js
+++ public/js/edit-nav.js
@@ -17,32 +17,104 @@ $(document).ready(function(){
 	catch(err){
 		console.log(err);
 	}
+	$('#nav_menu_form').submit(function(){
+		var nav = new Array();
+		
+		//Iterate over each nav item
+		$('#nav_list > .nav_item').each(function(){
+			//Build the tree for this item
+			var navTree = buildNavTree($(this));
+
+			//Push this item's tree to the nav list
+			nav.push(navTree);
+		});
+		
+		var csrf_token = $('input[name="csrf_token"]').val();
+		
+		$.post('/dashboard/nav', {'nav': nav, 'csrf_token' : csrf_token}, function(data){
+			
+			//Parse json string
+			data = JSON.parse(data);
+
+			$('#save_message').html(data.message);
+			if(data.success == true){
+				$('#save_message').addClass('alert-success');
+			}
+			else{
+				$('#save_message').addClass('alert-error');
+			}
+			
+			$('#save_message').removeClass('hidden');
+		});
+				
+		return false;
+	});
+	
+	//Adding items to Nav menu
+	$('#add-docs').click(function(){
+		//Add each checked item
+		$('.menu_item').each(function(){
+			//Retrieve the item's checkbox input
+			var checkbox = $(this).find('input[type="checkbox"]');
+			
+
+			//Only add the checked items
+			if(checkbox.is(':checked')){
+
+				//Get item's data
+				var label = $(this).find('.doc-title').text();
+				var slug = checkbox.val();
+				var type = $(this).find('input[name="type"]').val(); 
+
+				//Append item to nav list
+				var listItem = $('<li class="nav_item"></li>');
+				var sortHandle = $('<div class="sort_handle"></div>');
+				var deleteItem = $('<p class="delete_nav_item">x</p>').click(delete_nav_handler);
+				
+				sortHandle.append('<span>' + label + '</span>');
+				sortHandle.append('<input type="hidden" value="' + slug + '" name="link"/>');
+				sortHandle.append('<input type="hidden" value="' + type + '" name="type" />');
+				sortHandle.append(deleteItem);
+				
+				listItem.append(sortHandle);
+				
+				$('#nav_list').append(listItem);
+				//$('#nav_list').append('<li class="nav_item"><div class="sort_handle"><span>' + label + '</span><input type="hidden" value="/' + slug + '" /><p class="delete_nav_item">x</p></div></li>');
+
+				//Uncheck the item
+				checkbox.prop('checked', false);
+			}
+		});
+	});
+	
+	//Saving Navigation
+	$('#save_nav').click(function(){
+		var csrf_token = $('input[name="csrf_token"]');
+		
+		//Instantiate nav array
+		var nav = new Array();
+
+		//Iterate over each nav item
+		$('#nav_list > .nav_item').each(function(){
+			//Build the tree for this item
+			var navTree = buildNavTree($(this));
+
+			//Push this item's tree to the nav list
+			nav.push(navTree);
+		});
+
+		//Post the nav menu for saving
+		c
+	});
+
+	//Delete item from nav menu
+	$('.delete_nav_item').click(delete_nav_handler);
 });
 
-//Adding items to Nav menu
-$('#add_nav_items').click(function(){
-	
-	//Add each checked item
-	$('.menu_item').each(function(){
-		
-		//Retrieve the item's checkbox input
-		var checkbox = $(this).find('input[type="checkbox"]');
-		
-		//Only add the checked items
-		if(checkbox.is(':checked')){
-			
-			//Get item's data
-			var label = $(this).text();
-			var slug = checkbox.val();
-			
-			//Append item to nav list
-			$('#nav_list').append('<li class="nav_item"><div class="sort_handle"><span>' + label + '</span><input type="hidden" value="/' + slug + '" /><p class="delete_nav_item">x</p></div></li>');
-			
-			//Uncheck the item
-			checkbox.prop('checked', false);
-		}
-	});
-});
+function delete_nav_handler(){
+	var parent = $(this).parent('div').parent('.nav_item');
+	parent.remove();
+}
 
 //Returns the count of a parent's children
 function hasChildren(parent){
@@ -56,10 +128,11 @@ function buildNavTree(parent){
 	
 	//get nav item label and link
 	label = parent.children('div').children('span').text();
-	link = parent.children('div').children('input[type="hidden"]').val();
+	link = parent.children('div').children('input[name="link"]').val();
+	type = parent.children('div').children('input[name="type"]').val();
 	
 	//Create json object for nav item
-	ret = {'label' : label, 'link' : link};
+	ret = {'label' : label, 'link' : link, 'type' : type};
 	
 	if(!hasChildren(parent)){
 		return ret;
@@ -79,43 +152,3 @@ function buildNavTree(parent){
 	
 	return ret;
 }
-
-//Saving Navigation
-$('#save_nav').click(function(){
-	//Instantiate nav array
-	var nav = new Array();
-	
-	//Iterate over each nav item
-	$('#nav_list > .nav_item').each(function(){
-		//Build the tree for this item
-		var navTree = buildNavTree($(this));
-		
-		//Push this item's tree to the nav list
-		nav.push(navTree);
-	});
-	
-	//Post the nav menu for saving
-	$.post('admin-ajax.php', {'action':'save-nav', 'nav-menu': nav}, function(data){
-		//Parse json string
-		data = JSON.parse(data);
-		
-		//Check rows updated
-		if(data.rows > 0){//Save Successful
-			$('#save_message').html('Nav menu updated.');
-		}
-		else if(data.rows == 0){//No changes
-			$('#save_message').html('Nothing to update.');
-		}
-		else{//Error
-			$('#save_message').html('There was an error updating the nav menu.');
-		}
-		
-		$('#save_message').removeClass('hidden');
-	});
-});
-
-//Delete item from nav menu
-$('.delete_nav_item').click(function(){
-	var parent = $(this).parent('div').parent('.nav_item');
-	parent.remove();
-});
