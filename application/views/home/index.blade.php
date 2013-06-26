3f32316da0935f9f89c3c50de63f00fdcb9ada3f
Ryan Lombardo <ryan.lombardo@jouleww.com>
2013-06-25 21:34:59 -0400

    Updated structure of Home page content

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -1,11 +1,8 @@
-@layout('layouts/main')
+@layout('layouts.main')
+
 @section('content')
-<div class="row-fluid">
-	<div class="span12">
-		<h3>Welcome to the <strong>Madison Federal</strong> editing page</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Fusce in eros vel nibh hendrerit elementum nec id magna.  Aliquam sed elementum tellus.  Fusce vulputate pretium nisi in accumsan.  Donec tempus ornare enim.</p>
-		<h4>Ready to get started?</h4>
-		<p><a href="{{ URL::to('signup') }}">Signup</a> or <a href="{{ URL::to('login') }}">Login</a> if you already have an account.</p>
-	</div>
-</div>
+	<h1>Welcome to the <strong>Madison Federal</strong> editing page</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Fusce in eros vel nibh hendrerit elementum nec id magna.  Aliquam sed elementum tellus. Fusce vulputate pretium nisi in accumsan. Donec tempus ornare enim.</p>
+	<h2>Ready to get started?</h2>
+	<p><a href="{{ URL::to('signup') }}">Signup</a> or <a href="{{ URL::to('login') }}">Login</a> if you already have an account.</p>
 @endsection
