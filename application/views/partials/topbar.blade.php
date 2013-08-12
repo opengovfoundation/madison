74c8650e4a24dffda95bc99b85ba13ee30162762
Chris Birk <cmbirk@gmail.com>
2013-08-12 11:55:22 -0400

    start styling topbar

--- application/views/partials/topbar.blade.php
+++ application/views/partials/topbar.blade.php
@@ -1,13 +1,13 @@
 <div class="span4">
-	<select name="" id="">
+	<select name="" id="" style="margin-left:10px;">
 		<option value="">Select a recent bill</option>
 	</select>
 </div>
 <div class="span2">
-	<input type="button" value="Request a bill"/>
+	<input type="button" class="black" value="Request a Bill" style="margin-top:10px;"/>
 </div>
 <div class="span2">
-	<a href="">Advanced Bill Search &gt;&gt;</a>
+	<a href="" style="margin-top:15px; display:block; color:white;">Advanced Bill Search &gt;&gt;</a>
 </div>
 <div class="span4">
 	<input type="search" placeholder="Search"/>
