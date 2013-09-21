7423202f96110c529290a0e1f032554133391991
Ryan Lombardo <ryan.lombardo@buzzfeed.com>
2013-09-21 16:54:29 -0400

    Continued footer styles

--- application/views/layouts/footer.blade.php
+++ application/views/layouts/footer.blade.php
@@ -1,9 +1,7 @@
-<div class="nav nav-footer col-md-10 col-md-offset2">
-	<ul>
-		<li><a href="http://opengovfoundation.org" target="_blank">The OpenGov Foundation</a></li>
-		<li><a href="mailto:OpenGov Foundation <sayhello@opengovfoundation.org>?subject=Media Inquiry" target="_blank">Media Inquiries</a></li>
-		<li><a href="#">Contact</a></li>
-		<li><a href="#">Terms &amp; Conditions</a></li>
-		<li><a href="#">Report a Bug</a></li>
-	</ul>
+<div class="nav nav-footer col-md-12">
+	<a href="http://opengovfoundation.org" target="_blank">The OpenGov Foundation</a>
+	<a href="mailto:OpenGov Foundation <sayhello@opengovfoundation.org>?subject=Media Inquiry" target="_blank">Media Inquiries</a>
+	<a href="#">Contact</a>
+	<a href="#">Terms &amp; Conditions</a>
+	<a href="#">Report a Bug</a>
 </div>
