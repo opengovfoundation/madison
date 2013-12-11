557b1e0e41b4028dc000105315735f8db285e19a
Bill Hunt <bill.hunt@willowtreeapps.com>
2013-12-11 15:04:00 -0500

    Fixing links on homepage.

--- app/views/page/home.blade.php
+++ app/views/page/home.blade.php
@@ -1,4 +1,4 @@
 <h1>Welcome to the <strong>Madison</strong></h1>
 <p>Home page content. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in eros vel nibh hendrerit elementum nec id magna.  Aliquam sed elementum tellus. Fusce vulputate pretium nisi in accumsan. Donec tempus ornare enim.</p>
 <p>Ready to get started?</p>
-<p><a href="{{ URL::to('signup') }}" class="red">Signup</a> or <a href="{{ URL::to('login') }}" class="red">Login</a> if you already have an account.</p>
+<p><a href="{{ URL::to('user/signup') }}" class="red">Signup</a> or <a href="{{ URL::to('user/login') }}" class="red">Login</a> if you already have an account.</p>
