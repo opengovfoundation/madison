068b59236f5e0f9d48d8a2cf38856e138d192b60
Chris Birk <cmbirk@gmail.com>
2014-01-02 12:46:30 -0500

    add annotator when not logged in (testing)

--- app/views/doc/reader/index.blade.php
+++ app/views/doc/reader/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts/main')
 @section('content')
-	@if(Auth::check())
+	{{-- @if(Auth::check()) --}}
 	{{ HTML::style('vendor/annotator/annotator.min.css') }}
 	{{ HTML::script('vendor/annotator/annotator-full.min.js') }}
 	{{ HTML::script('vendor/showdown/showdown.js') }}
 	{{ HTML::script('js/doc.js') }}
-	@endif
+	{{-- @endif --}}
 	<div id="content" class="col-md-8 content doc_content @if(Auth::check())logged_in@endif">
 		<div class="row">
 			<div class="col-md-12">
