0d6b3bf92c0b85a926a786172177bb2e8200206c
Chris Birk <cmbirk@gmail.com>
2014-01-08 15:45:26 -0500

    clean up plugin structure

--- public/js/annotator-madison.js
+++ public/js/annotator-madison.js
@@ -1,18 +1,25 @@
-Annotator.Plugin.Madison = function(element){
-	var madison = {};
+Annotator.Plugin.Madison = function(element, options){}
 
-	madison.pluginInit = function(){
+$.extend(Annotator.Plugin.Madison.prototype, new Annotator.Plugin(), {
+	events: {},
+	options: {},
+	pluginInit: function(){
+		console.log(this.annotator);
 		this.annotator.viewer.addField({
 			load: function(field, annotation){
+				console.log(annotation);
+				
 
-				//Add link to note in Madison
+				//Add link to annotation in Madison
 				noteLink = $('<div class="annotation-link"></div>');
 				annotationLink = $('<a></a>').attr('href', window.location.origin + '/note/' + annotation.id).text('View Note');
 				noteLink.append(annotationLink);
-
 				$(field).append(noteLink);
+
+				//Add actions to annotation
 				annotationAction = $('<div></div>').addClass('annotation-action');
 				generalAction = $('<span></span>').addClass('glyphicon').data('annotation-id', annotation.id);
+				
 				annotationLike = generalAction.clone().addClass('glyphicon-thumbs-up');
 				annotationDislike = generalAction.clone().addClass('glyphicon-thumbs-down');
 				annotationFlag = generalAction.clone().addClass('glyphicon-flag');
@@ -23,6 +30,5 @@ Annotator.Plugin.Madison = function(element){
 			}
 		})
 	}
-
-	return madison;
-}
+});
+	
