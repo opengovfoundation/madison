79b82447bc0b588e2f7d7c2147de9bdfe643e613
Chris Birk <cmbirk@gmail.com>
2014-01-17 00:53:21 -0600

    hide comments section if there are no comments

--- public/js/annotator-madison.js
+++ public/js/annotator-madison.js
@@ -136,10 +136,17 @@ $.extend(Annotator.Plugin.Madison.prototype, new Annotator.Plugin(), {
 
 				$(field).append(annotationAction);
 
+				console.log($(annotation.comments).length);
+
 				commentsHeader = $('<div class="comment-toggle" data-toggle-"collapse" data-target="#current-comments">Comments <span id="comment-caret" class="caret caret-right"></span></button>').click(function(){
 					$('#current-comments').collapse('toggle');
 					$('#comment-caret').toggleClass('caret-right');
 				});
+
+				if($(annotation.comments).length == 0){
+					commentsHeader.addClass('hidden');
+				}
+
 				currentComments = $('<div id="current-comments" class="current-comments collapse"></div>');
 				$.each(annotation.comments, function(index, comment){
 					comment = $('<div class="existing-comment"><blockquote>' + comment.text + '<div class="comment-author">' + comment.user.name + '</div></blockquote></div>');
@@ -167,6 +174,7 @@ $.extend(Annotator.Plugin.Madison.prototype, new Annotator.Plugin(), {
 							annotation.comments.push(comment);
 							comment = $('<div class="existing-comment"><blockquote>' + comment.text + '<div class="comment-author">' + comment.user.name + '</div></blockquote></div>');
 							currentComments.append(comment);
+							currentComments.removeClass('hidden');
 						});
 
 						$('#current-comments').collapse(true);
