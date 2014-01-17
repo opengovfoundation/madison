27b81ddc2bd8ec9cc3f4d41ef1f45c080f859617
Chris Birk <cmbirk@gmail.com>
2014-01-16 23:02:15 -0600

    disable ApiController getIndex for now because AnnotationApiController::getIndex is not compatible

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -17,31 +17,31 @@ class ApiController extends BaseController{
 		$this->es = new Elasticsearch\Client($params);
 	}
 	
-	public function getIndex(){
-		$api = array(
-			'message' 	=> 'Madison API',
-			'links'		=> array(
-			    'annotation'	=> array(
-                    'create'	=> array(
-						'url'	=> action('AnnotationApiController@postIndex')
-					),
-			    	'read'		=> array(
-			    		'url'	=> action('AnnotationApiController@getIndex', array('id'))
-			    	),
-			    	'update'	=> array(
-						'url'	=> action('AnnotationApiController@putIndex', array('id'))
-					),
-					'destroy'	=> array(
-						'url'	=> action('AnnotationApiController@deleteIndex', array('id'))
-					),
-					'search'	=> array(
-						'url'	=>action('AnnotationApiController@getSearch')
-					)
-			    ),
-			)
-		);
+	// public function getIndex(){
+	// 	$api = array(
+	// 		'message' 	=> 'Madison API',
+	// 		'links'		=> array(
+	// 		    'annotation'	=> array(
+ //                    'create'	=> array(
+	// 					'url'	=> action('AnnotationApiController@postIndex')
+	// 				),
+	// 		    	'read'		=> array(
+	// 		    		'url'	=> action('AnnotationApiController@getIndex', array('id'))
+	// 		    	),
+	// 		    	'update'	=> array(
+	// 					'url'	=> action('AnnotationApiController@putIndex', array('id'))
+	// 				),
+	// 				'destroy'	=> array(
+	// 					'url'	=> action('AnnotationApiController@deleteIndex', array('id'))
+	// 				),
+	// 				'search'	=> array(
+	// 					'url'	=>action('AnnotationApiController@getSearch')
+	// 				)
+	// 		    ),
+	// 		)
+	// 	);
 
-		return Response::json($api);
-	}
+	// 	return Response::json($api);
+	// }
 }
 
