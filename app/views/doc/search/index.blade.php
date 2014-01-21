7004df0e6d5ee256816d1f27b42e6777583c042c
Chris Birk <cmbirk@gmail.com>
2014-01-21 12:44:48 -0600

    add display for no matching results

--- app/views/doc/search/index.blade.php
+++ app/views/doc/search/index.blade.php
@@ -2,10 +2,15 @@
 @section('content')
 	<div class="content col-md-12 docs">
 		<h1>Search Results</h1>
-		<p>Documents Matching:</p>
-		<blockquote>&quot;{{ $query }}&quot;</blockquote>
-		@foreach( $results as $result )
-			<a href="{{ URL::to('docs/' . $result->slug) }}">{{ $result->title }}</a>
-		@endforeach
+		@if(count($results) == 0)
+			<p>No documents matched:</p>
+			<blockquote>&quot;{{ $query }}&quot;</blockquote>
+		@else
+			<p>Documents Matching:</p>
+			<blockquote>&quot;{{ $query }}&quot;</blockquote>
+			@foreach( $results as $result )
+				<a href="{{ URL::to('docs/' . $result->slug) }}">{{ $result->title }}</a>
+			@endforeach
+		@endif
 	</div>
 @endsection
