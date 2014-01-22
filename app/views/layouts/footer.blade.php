9c1d2a70b40a9dd06cdc5a1d24b8c72941253c9a
Chris Birk <cmbirk@gmail.com>
2014-01-21 20:46:41 -0500

    remove unnecessay footer links

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -1,7 +1,5 @@
 <div class="nav nav-footer col-md-12">
 	<a href="http://opengovfoundation.org" target="_blank">The OpenGov Foundation</a>
 	<a href="mailto:OpenGov Foundation <sayhello@opengovfoundation.org>?subject=Media Inquiry" target="_blank">Media Inquiries</a>
-	<a href="#">Contact</a>
 	<a href="#">Terms &amp; Conditions</a>
-	<a href="#">Report a Bug</a>
 </div>
