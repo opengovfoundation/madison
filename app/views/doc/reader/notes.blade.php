7d7a804dda971fe5e5a955b0e96bc3a72c8154c6
Chris Birk <cmbirk@gmail.com>
2014-02-03 15:10:14 -0500

    add links to annotation / comment authors

--- app/views/doc/reader/notes.blade.php
+++ app/views/doc/reader/notes.blade.php
@@ -3,8 +3,13 @@
 	<div class="sidebar-annotation" ng-repeat="annotation in annotations">
 		<blockquote>
 			<a href="/note/<% annotation.id %>">
-				<div class="annotation-content" ng-bind-html="annotation.html"></div><div class="annotation-author"><% annotation.user.name %></div>
+				<div class="annotation-content" ng-bind-html="annotation.html"></div>
 			</a>
+			<div class="annotation-author">
+				<a href="/user/<% annotation.user.id %>">
+					<% annotation.user.name %>
+				</a>
+			</div>
 			<span class="sidebar-comment-label" ng-show="annotation.comments" ng-click="showCommentThread(annotation.id, $event)">Comments (<% comments.length %>)<span class="sidebar-comment-caret caret caret-right"></span></span>
 		</blockquote>
 		
@@ -12,7 +17,11 @@
 			<div class="sidebar-comment" ng-repeat="comment in annotation.comments">
 				<blockquote>
 					<span class="comment-content"><% comment.text %></span>
-					<div class="comment-author"><% comment.user.name %></div>
+					<div class="comment-author">
+						<a href="/user/<% comment.user.id %>">
+							<% comment.user.name %>
+						</a>
+					</div>
 				</blockquote>
 			</div>
 		</div>
