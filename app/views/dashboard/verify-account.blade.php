c4c4f73207877edd2d562b4d00a2335fabfc5c5f
Chris Birk <cmbirk@gmail.com>
2014-02-11 18:40:46 -0500

    style verify account page

--- app/views/dashboard/verify-account.blade.php
+++ app/views/dashboard/verify-account.blade.php
@@ -11,21 +11,30 @@
 	<div class="row">
 		<div class="content col-md-12" ng-controller="DashboardVerifyController" ng-init="init()">
 			<h1>Verify Users</h1>
-			<ul>
-				@foreach($requests as $request)
-				<li ng-repeat="request in requests">
-					<span><% request.user.fname %> <% request.user.lname %> : </span>
-					<span><% request.meta_value %></span>
-					<span>
-						<div class="btn-group">
-							<button type="button" class="btn btn-success" ng-class="{active: request.meta_value == 'verified'}" ng-click="update(request, 'verified')">Verified</button>
-							<button type="button" class="btn btn-warning" ng-class="{active: request.meta_value == 'pending'}" ng-click="update(request, 'pending')">Pending</button>
-							<button type="button" class="btn btn-danger" ng-class="{active: request.meta_value == 'denied'}" ng-click="update(request, 'denied')">Denied</button>
-						</div>
-					</span>
-				</li>
-				@endforeach
-			</ul>
+			<table class="table table-striped">
+				<thead>
+					<th>ID</th>
+					<th>First Name</th>
+					<th>Last Name</th>
+					<th>Email</th>
+					<th>Status</th>
+				</thead>
+				<tbody>
+					<tr ng-repeat="request in requests">
+						<td><% request.id %></td>
+						<td><% request.user.fname%></td>
+						<td><% request.user.lname %></td>
+						<td><% request.user.email %></td>
+						<td>
+							<div class="btn-group">
+								<button type="button" class="btn btn-success" ng-class="{active: request.meta_value == 'verified'}" ng-click="update(request, 'verified')">Verified</button>
+								<button type="button" class="btn btn-warning" ng-class="{active: request.meta_value == 'pending'}" ng-click="update(request, 'pending')">Pending</button>
+								<button type="button" class="btn btn-danger" ng-class="{active: request.meta_value == 'denied'}" ng-click="update(request, 'denied')">Denied</button>
+							</div>
+						</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</div>
 	
