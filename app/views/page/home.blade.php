88190618f237bb9d2018a3cf97beb3a58e4f71e6
Chris Birk <cmbirk@gmail.com>
2014-02-17 22:02:14 -0500

    remove stray text

--- app/views/page/home.blade.php
+++ app/views/page/home.blade.php
@@ -1,15 +1,17 @@
-<h1>Welcome to Madison</h1>
-
-
-<ul ng-controller="RecentDocsController" ng-init="init()">
-	<li ng-repeat="doc in docs">
-		<a href="/docs/<% doc.slug %>">
-			<% doc.title %>
-		</a>
-		<div class="list-doc-info">
-			<span class="doc-created-date">Posted <% doc.created_at | date:'mediumDate' %></span>
-			<span class="doc-updated-date">Updated <% doc.updated_at | date:'mediumDate' %></span>
-			<span class="doc-action-count">doc.annotations.length</span>
-		</div>
-	</li>
-</ul>
+<div class="row">
+	<h1>Welcome to Madison</h1>
+</div>
+<div class="row">
+	<ul ng-controller="RecentDocsController" ng-init="init()">
+		<li ng-repeat="doc in docs">
+			<a href="/docs/<% doc.slug %>">
+				<% doc.title %>
+			</a>
+			<div class="list-doc-info">
+				<span class="doc-created-date">Posted <% doc.created_at | date:'mediumDate' %></span>
+				<span class="doc-updated-date">Updated <% doc.updated_at | date:'mediumDate' %></span>
+				<span class="doc-action-count"></span>
+			</div>
+		</li>
+	</ul>
+</div>
