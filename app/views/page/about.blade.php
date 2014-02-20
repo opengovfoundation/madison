b97a000740cf6dbfd912fd39761eac37f4a1349d
Chris Birk <cmbirk@gmail.com>
2014-02-19 19:00:22 -0500

    add about page content

--- app/views/page/about.blade.php
+++ app/views/page/about.blade.php
@@ -1,2 +1,30 @@
-<h1>About the Madison Platform</h1>
-<p>About content. Lorem ipsum dolor sit amet.</p>
+<h1>About Madison</h1>
+<p>Madison is a government policy co-creation platform that opens up laws and legislation previously off-limits to individuals and the Internet community.  Launched to battle the Stop Online Piracy Act (SOPA), it has since been used to power citizen participation in official government documents in the United States Congress. With Madison you can access the law as it’s being written, leave comments, annotate specific content, and interact with other civic-minded participants. Madison brings the law-making process straight to you, and gives you a say in your government’s decisions. </p>
+
+<strong>How it works:</strong>
+<ol>
+	<li>Sign up and create a username. </li>
+	<li>Verify your email address. </li>
+	<li>Locate a document by browsing through titles, filtering by sponsor, or searching for keywords. </li>
+	<li>Annotate, comment on, and support or oppose the document. </li>
+	<li>Stay connected by signing up for email updates.</li>
+</ol>
+
+<p>When you support or oppose a document, enter a comment, or annotate a specific section of a work in progress, the document’s sponsor receives notification electronically. Your interaction with legislation on Madison will keep sponsors informed on how their constituents feel about their work; they may also choose to incorporate suggested edits straight into their documents. </p>
+
+<strong>What’s next?</strong>
+
+<p>Engage with the community. Madison allows you to interact not only with specific pieces of legislation, but with other civic-minded citizens. See what other people are saying and share your thoughts. Explore documents from other sponsors or legislative bodies. And suggest changes or features you’d like to see on Madison by giving us feedback in the tab at the left-hand side of the page, or by sending us an email. We’d love to hear from you. </p>
+
+<strong>Coming Soon</strong>
+
+<p>Planned Madison featured and functionalities include:</p>
+<ul>
+	<li>A reputation system where users gain points for activities on Madison, including annotations, conversations, and document incorporations</li>
+	<li>Helpful information on documents posted on the site, sourced from Google news, third-party reviewers, and social media</li>
+	<li>Snapshots of documents to show their progression over time </li>
+</ul>
+
+<p>And more!</p>
+
+<p>Still have a question? Let us know on the left.  We'll get back to you right away.</p>
