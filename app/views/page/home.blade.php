e30ec123dddd470e34a182e1d9cfdead6ecb6190
Chris Birk <cmbirk@gmail.com>
2014-02-19 21:58:17 -0500

    filter unique categories and sponsors

--- app/views/page/home.blade.php
+++ app/views/page/home.blade.php
@@ -5,10 +5,10 @@
 	<select ui-select2="select2Config" ng-model="select2" placeholder="Filter documents by category or sponsor">
 		<option value=""></option>
 		<optgroup label="Category">
-			<option value="<% category %>" ng-repeat="category in categories"><% category %></option>
+			<option value="<% category %>" ng-repeat="category in categories | unique"><% category %></option>
 		</optgroup>
 		<optgroup label="Sponsor">
-			<option value="<% sponsor.id %>" ng-repeat="sponsor in sponsors"><% sponsor.fname %> <% sponsor.lname %></option>
+			<option value="<% sponsor.id %>" ng-repeat="sponsor in sponsors | unique"><% sponsor.fname %> <% sponsor.lname %></option>
 		</optgroup>
 	</select>
 	<ul>
