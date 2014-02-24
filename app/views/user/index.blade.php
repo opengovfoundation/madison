4b7b7f7e669d3f2355f763f759940b82d346445e
Chris Birk <cmbirk@gmail.com>
2014-02-24 14:20:53 -0500

    update user page markup

--- app/views/user/index.blade.php
+++ app/views/user/index.blade.php
@@ -1,19 +1,11 @@
 @extends('layouts.main')
 @section('content')
 	<div class="content col-md-12" ng-controller="UserPageController" ng-init="init()">
-		<div class="row">
-			<div class="col-md-2">
-				<img ng-src="http://www.gravatar.com/avatar/<% user.email | gravatar %>" class="img-rounded img-responsive" alt="" />
-			</div>
-			<div class="col-md-10">
-				<div class="row">
-					<h1 class="user-name"><% user.fname %> <% user.lname %></h1>
-					<span class="user-verified" ng-show="verified">Verified</span>	
-				</div>
-				<div class="row">
-					<span class="user-created-date">Member since <% user.created_at | date:'mediumDate'  %></span>
-				</div>
-			</div>
+		<div class="row user-info">
+			<img ng-src="http://www.gravatar.com/avatar/<% user.email | gravatar %>" class="img-rounded img-responsive user-gravatar" alt="" />
+			<h1 class="user-name"><% user.fname %> <% user.lname %></h1>
+			<span class="user-verified" ng-show="verified">Verified</span>	
+			<span class="user-created-date">Member since <% user.created_at | date:'mediumDate'  %></span>
 		</div>
 		<div class="row">
 			<tabset justified="true">
