1cde130b12945e52c902a9b29e361033d6492e25
Chris Birk <cmbirk@gmail.com>
2014-04-06 10:51:28 -0700

    add browsehappy prompt

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -13,6 +13,9 @@
 		@include('layouts.assets')
 	</head>
 	<body>
+		<!--[if lt IE 8]>
+			<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/" target="_blank">upgrade your browser</a> to improve your experience.</p>
+		<![endif]-->
 		<div id="wrap" class="wrap">
 			<div id="header-main" class="header row">
 				<div class="container">
