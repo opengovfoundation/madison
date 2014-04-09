343e2119f9e5be18deeb9846a73c244e496a0c8d
Chris Birk <cmbirk@gmail.com>
2014-04-09 00:00:14 -0400

    start adding test files

---
+++ app/tests/AnnotationTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Way\Tests\Assert;
+use Way\Tests\Should;
+
+class AnnotationTest extends TestCase {
+
+	//TODO: methods to test
+		//getEsClient
+		//setEsIndex
+		//getEsIndex
+		//connectToEs
+		//user
+		//comments
+		//tags
+		//permissions
+		//createFromAnnotatorArray
+		//toAnnotatorArray
+		//loadAnnotationsForAnnotator
+		//updateSearchIndex
+		//delete
+		//addOrUpdateComment
+		//getMetaCount
+		//saveUserAction
+		//likes
+		//dislikes
+		//flags
+	
+}
---
+++ app/tests/UserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Way\Tests\Assert;
+use Way\Tests\Should;
+
+class PageTest extends TestCase {
+
+	//TODO: methods to test
+		//verified
+		//docs
+		//comments
+		//annotations
+		//getAuthIdentifier
+		//getAuthPassword
+		//getReminderEmail
+		//organizations
+		//note_meta
+		//user_meta
+		//admin_contact
+		//doc_meta
+}
