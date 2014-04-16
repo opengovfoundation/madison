fa8f4b4f95a62de8975ce94e80127bd644ef6ea0
Chris Birk <cmbirk@gmail.com>
2014-04-16 16:16:55 -0400

    separate dashboard controllers, lint controller js files and gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,78 +1,68 @@
-//Gruntfile
 module.exports = function (grunt) {
 
-	//Initializing the configuration object
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-		// Task configuration
-		compass: {
-			dist: {
-				options: {
-					config: './public/config.rb',
-					environment: 'production'
-				}
-			},
-			dev: {
-				options: {
-					config: './public/config.rb',
-				}
-			}
-		},
-		jshint: {
-			options: {
-				force: true
-			}
-		},
-		browserify: {
-			js: {
-				//Single entry file for frontend app
-				src: 'public/js/app.js',
-				//Compiles to a single script
-				dest: 'public/build/app.js'
-			}
-		},
-		uglify: {
-			options: {
-				mangle: false
-			},
-			components: {
-				files: {
-					'./public/components.js': './public/components.js'
-				}
-			},
-			main: {
-				files: {
-					'./public/main.js': './public/main.js'
-				}
-			}
-		},
-		watch: {
-			scripts: {
-				files: ['public/js/*.js'],
-				tasks: ['browserify']
-			}
-			// sass: {
-			// 	files: './public/sass/**/*.scss',
-			// 	tasks: ['compass']
-			// },
-			// livereload: {
-			// 	files: ['./public/css/*.css', './public/img/*', './public/js/**/*.js'],
-			// 	options: {
-			// 		livereload: true
-			// 	}
-			// }
-		}
-	});
+    // Task configuration
+    compass: {
+      dist: {
+        options: {
+          config: './public/config.rb',
+          environment: 'production'
+        }
+      },
+      dev: {
+        options: {
+          config: './public/config.rb',
+        }
+      }
+    },
+    jshint: {
+      options: {
+        force: true
+      }
+    },
+    browserify: {
+      js: {
+        src: 'public/js/app.js',
+        dest: 'public/build/app.js'
+      }
+    },
+    uglify: {
+      options: {
+        mangle: false
+      },
+      components: {
+        files: {
+          './public/components.js': './public/components.js'
+        }
+      },
+      main: {
+        files: {
+          './public/main.js': './public/main.js'
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['public/js/*.js'],
+        tasks: ['browserify']
+      },
+      sass: {
+        files: './public/sass/**/*.scss',
+        tasks: ['compass']
+      }
+    }
+  });
 
-	// Plugin loading
-	grunt.loadNpmTasks('grunt-contrib-compass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-browserify');
+  // Plugin loading
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-browserify');
 
-	// Task definition
-	grunt.registerTask('default', ['browserify', 'watch']);
+  // Task definition
+  grunt.registerTask('default', ['browserify', 'watch']);
 };
--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,37 @@
-// Hacks to get build process working.
-// TODO: These need to be cleaned up.
 window.$ = require('jquery');
 window.jQuery = window.$;
 window.Markdown = require('pagedown');
+
 
 require('select2-browserify');
 require('underscore');
 require('angular');
 require('angular-bootstrap');
 require('angular-animate');
-require('../bower_components/angular-ui/build/angular-ui.min.js');
+require('../../bower_components/angular-ui/build/angular-ui.min.js');
+require('../../node_modules/twitter-bootstrap-3.0.0/dist/js/bootstrap.min.js');
 
 //Require custom angular modules
 require('./controllers');
+require('./dashboardControllers');
 require('./services');
 require('./directives');
 require('./filters');
+
 window.getAnnotationService = require('./annotationServiceGlobal');
 
-imports = [
+var imports = [
     'madisonApp.filters',
     'madisonApp.services',
     'madisonApp.directives',
     'madisonApp.controllers',
+    'madisonApp.dashboardControllers',
     'ui',
     'ui.bootstrap',
     'ngAnimate',
-];
+  ];
 
-var app = angular.module('madisonApp', imports, function ($interpolateProvider){
+var app = angular.module('madisonApp', imports, function ($interpolateProvider) {
     $interpolateProvider.startSymbol('<%');
     $interpolateProvider.endSymbol('%>');
-})
-.config;
+  });
