e61c85e33b35dc58b481062d9e091e61e04f42b9
Chris Birk <cmbirk@gmail.com>
2014-05-22 21:24:48 -0400

    style document filters on the homepage

--- app/views/page/home.blade.php
+++ app/views/page/home.blade.php
@@ -5,29 +5,27 @@
 	<div class="col-sm-6">
 		<input type="text" ng-model="docSearch" class="form-control" placeholder="Search document titles">
 	</div>
-	<div class="col-sm-4 col-sm-offset-2">
-		<div class="row">
-			<select ui-select2="select2Config" ng-model="select2">
-				<option value=""></option>
-				<optgroup label="Category">
-					<option value="@{{ category }}" ng-repeat="category in categories">@{{ category }}</option>
-				</optgroup>
-				<optgroup label="Sponsor">
-					<option value="@{{ sponsor.id }}" ng-repeat="sponsor in sponsors">@{{ sponsor.fname }} @{{ sponsor.lname }}</option>
-				</optgroup>
-				<optgroup label="Status">
-					<option value="@{{ status.id}}" ng-repeat="status in statuses">@{{ status.label}}</option>
-				</optgroup>
-			</select>
-		</div>
-		<div class="row">
+	<div class="col-sm-4 home-select2-container">
+		<select ui-select2="select2Config" ng-model="select2">
+			<option value=""></option>
+			<optgroup label="Category">
+				<option value="@{{ category }}" ng-repeat="category in categories">@{{ category }}</option>
+			</optgroup>
+			<optgroup label="Sponsor">
+				<option value="@{{ sponsor.id }}" ng-repeat="sponsor in sponsors">@{{ sponsor.fname }} @{{ sponsor.lname }}</option>
+			</optgroup>
+			<optgroup label="Status">
+				<option value="@{{ status.id}}" ng-repeat="status in statuses">@{{ status.label}}</option>
+			</optgroup>
+		</select>
+	</div>
+	<div class="col-sm-2 home-select2-container">
 			<select ui-select2="dateSortConfig" id="dateSortSelect" ng-model="dateSort">
 				<option value=""></option>
 				<option value="created_at">Date Posted</option>
 				<option value="updated_at">Last Updated</option>
 			</select>
 		</div>
-	</div>
 	<div class="col-sm-12">
 		<ul>
 			<li ng-repeat="doc in docs | toArray | filter:docSearch | orderBy:dateSort:reverse" ng-show="docFilter(doc)">
