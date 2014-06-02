29aa023d4b0f4a2f2a216af2986bbaeec87c548e
Chris Birk <cmbirk@gmail.com>
2014-06-02 11:55:48 -0400

    add subscribe link to header

--- app/views/layouts/header.blade.php
+++ app/views/layouts/header.blade.php
@@ -9,18 +9,16 @@
 		<li class="link-about"><a href="{{ URL::to('about') }}">About</a></li>
 		<li class="link-faq"><a href="{{ URL::to('faq') }}">FAQ</a></li>	
 		<li class="link-support"><a href="https://rally.org/opengovfoundation" target="_blank">Donate</a></li>	
+		<li class="link-subscribe"><a href="http://opengovfoundation.us6.list-manage.com/subscribe?u=9d450bf68b3df1185fc9f62b2&id=40a5a16e19" target="_blank">Subscribe</a></li>
 		@if(Auth::check())
 			<li class="dropdown">
 				<a class="dropdown-trigger" href="#" data-toggle="dropdown">Welcome {{ Auth::user()->fname }} <span class="caret"></span></a>
 				<ul class="dropdown-menu" role="menu">
-					<!-- <li class="link-bookmarked"><a href="#" class="disabled coming-feature">Bookmarked Documents</a></li> -->
-					<!-- <li class="link-points"><a href="#" class="disabled coming-feature">Your Points</a></li> -->
 					<li class="link-settings"><a href="{{ URL::to('user/edit/' . Auth::user()->id) }}">Account Settings</a></li>
 					<li class="link-settings"><a href="{{ URL::to('groups') }}">Group Management</a></li>
 					@if(Auth::user()->hasRole('Admin'))
 					<li><a href="{{ URL::to('dashboard') }}">Administrative Dashboard</a></li>
 					@endif
-					<!-- <li class="link-help"><a href="#" class="disabled coming-feature">Help</a></li> -->
 					<li class="link-logout"><a href="{{ URL::to('logout') }}">Logout</a></li>
 				</ul>
 			</li>
