0daef792de024cd7ea372e0b67d049927bdda4ac
Chris Birk <cmbirk@gmail.com>
2014-06-11 13:26:25 -0400

    add DualMigration model for SQLite compatible migrations

---
+++ app/models/DualMigration.php
@@ -0,0 +1,48 @@
+<?php
+  
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+abstract class DualMigration extends Migration implements DualMigrationInterface {
+
+  public function up(){
+    $connection = DB::connection()->getDriverName();
+
+    switch($connection){
+      case 'mysql':
+        $this->upMySQL();
+        break;
+
+      case 'sqlite':
+        $this->upSQLite();
+        break;
+
+      default: 
+        throw new Exception("Unknown connection $connection");
+    }
+  }
+
+  public function down(){
+    $connection = DB::connection()->getDriverName();
+
+    switch($connection){
+      case 'mysql':
+        $this->downMySQL();
+        break;
+
+      case 'sqlite':
+        $this->downSQLite();
+        break;
+
+      default: 
+        throw new Exception("Unknown connection $connection");
+    }
+  }
+
+  abstract public function upMySQL();
+  abstract public function downMySQL();
+  abstract public function upSQLite();
+  abstract public function downSQLite();
+}
+
+
