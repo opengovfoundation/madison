17d4a32b98fbb205a4193cff383e349511e25dfc
Chris Birk <cmbirk@gmail.com>
2014-07-01 13:54:51 -0400

    start moving from can('independent_sponsor...') to hasRole('Independent Sponsor') and creating interface

--- app/views/documents/list.blade.php
+++ app/views/documents/list.blade.php
@@ -7,31 +7,36 @@
 		</ol>
 	</div>
 	<div class="row content">
-		<div class="col-md-8 admin-document-list">
-			<h2>Documents</h2>
-			<ul>
-				@if(0 == count($docs))
-					<li>No Documents Found</li>
-				@else
-					@foreach($docs as $doc)
-						<li>
-							<?php echo HTML::link('documents/edit/' . $doc->id, $doc->title); ?>
-						</li>
-					@endforeach
-				@endif
-
-			</ul>
-		</div>
-		<div class="col-md-4 admin-add-documents">
-			<div class="row">
-				<h2>Create Document</h2>
-				{{ Form::open(array('url' => 'documents/create', 'method' => 'post', 'id' => 'create-document-form')) }}
-				<div class="form-group">
-					{{ Form::label('title', 'Title:') . Form::text('title', Input::old('title'), array('placeholder' => 'Document Title', 'class'=>'form-control')) }}
-				</div>
-				{{ Form::submit('Create Document', array('class' => 'btn')) }}
-				{{ Form::token() . Form::close() }}
+		@if(Auth::user()->hasRole('Independent Sponsor'))
+			<div class="col-md-8 admin-document-list">
+				<h2>Documents</h2>
+				<ul>
+					@if(0 == count($docs))
+						<li>No Documents Found</li>
+					@else
+						@foreach($docs as $doc)
+							<li>
+								<?php echo HTML::link('documents/edit/' . $doc->id, $doc->title); ?>
+							</li>
+						@endforeach
+					@endif
+				</ul>
 			</div>
-		</div>
+			<div class="col-md-4 admin-add-documents">
+				<div class="row">
+					<h2>Create Document</h2>
+					{{ Form::open(array('url' => 'documents/create', 'method' => 'post', 'id' => 'create-document-form')) }}
+					<div class="form-group">
+						{{ Form::label('title', 'Title:') . Form::text('title', Input::old('title'), array('placeholder' => 'Document Title', 'class'=>'form-control')) }}
+					</div>
+					{{ Form::submit('Create Document', array('class' => 'btn')) }}
+					{{ Form::token() . Form::close() }}
+				</div>
+			</div>
+		@else
+			<div class="col-md-8">
+				<p>Want to be a document sponsor? <a href="/documents/sponsor/request">Request to be an Independent Sponsor</a></p>
+			</div>
+		@endif
 	</div>
 @endsection
