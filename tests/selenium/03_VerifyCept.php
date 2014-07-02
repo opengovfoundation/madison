282b8dc0797757efde74d58ae1065c9e1e39cc9d
Rostislav Tsiomenko <rostislav.tsiomenko@gmail.com>
2014-07-02 17:25:33 -0400

    Turned on cleanup, added fixtures

--- tests/selenium/03_VerifyCept.php
+++ tests/selenium/03_VerifyCept.php
@@ -1,5 +1,11 @@
 <?php 
 $I = new SeleniumTester($scenario);
+$I->haveInDatabase('users', array('email' => 'codeception@gmail.com', 
+								  'password' => '$2y$10$sESIh1sRtuINotOAPXsjOeVSXQ8wpW/vi4yLnnunKTTrkCfRpIi3W', 
+								  'fname' => 'Codeception', 
+								  'lname' => 'McIntire', 
+								  'token' => '7IU1u49GhrBsY5oP')
+                  );
 $I->wantTo('check that user can verify account');
 $I->amOnPage('/');
 $token = $I->grabFromDatabase('users', 'token', array('email' => 'codeception@gmail.com'));
