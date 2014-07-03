1b9aafeb25a8dae9ae20ecbe4854d79ed0c73796
Rostislav Tsiomenko <rostislav.tsiomenko@gmail.com>
2014-07-03 15:35:08 -0400

    Moving signin testing to two separate tests

--- tests/acceptance/SigninNoVerify.php
+++ tests/acceptance/SigninNoVerify.php
@@ -4,13 +4,13 @@ $I->haveInDatabase('users', array('email' => 'test@opengovfoundation.org',
 								  'password' => '$2y$10$sESIh1sRtuINotOAPXsjOeVSXQ8wpW/vi4yLnnunKTTrkCfRpIi3W', 
 								  'fname' => 'Codeception', 
 								  'lname' => 'McIntire', 
-								  'token' => '')
+								  'token' => '7IU1u49GhrBsY5oP')
                   );
-$I->wantTo('check that user can login normally');
+$I->wantTo('check that user cannot login without verifying account');
 $I->amOnPage('/');
 $I->amOnPage('/user/login');
 $I->fillField('Email', 'test@opengovfoundation.org');
 $I->fillField('Password', 'codeception');
 $I->click('Login', ['class' => 'btn']);
-$I->see('Welcome Codeception');
+$I->see('Please click the link sent to your email');
 ?>
