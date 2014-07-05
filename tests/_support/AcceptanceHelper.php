eb944e1e38d29c1a5046577f95d93116776d4f5b
Rostislav Tsiomenko <rostislav.tsiomenko@gmail.com>
2014-07-05 13:34:44 -0400

    Added create test user function

--- tests/_support/AcceptanceHelper.php
+++ tests/_support/AcceptanceHelper.php
@@ -6,5 +6,15 @@ namespace Codeception\Module;
 
 class AcceptanceHelper extends \Codeception\Module
 {
-
+	function createTestUser() {
+		// Insert test user into database
+		$dbh = $this->getModule('Db');
+		$dbh->haveInDatabase('users', 
+							 array('email' => 'test@opengovfoundation.org', 
+						     	   'password' => '$2y$10$sESIh1sRtuINotOAPXsjOeVSXQ8wpW/vi4yLnnunKTTrkCfRpIi3W', 
+						           'fname' => 'Codeception', 
+							       'lname' => 'McIntire', 
+							       'token' => '')
+                            );
+	}
 }
