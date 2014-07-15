f52af1e2e200da254a8d4574367399d09e68b57f
Chris Birk <cmbirk@gmail.com>
2014-07-15 19:54:33 -0400

    add some basic styling

--- app/views/dashboard/notifications.blade.php
+++ app/views/dashboard/notifications.blade.php
@@ -7,13 +7,14 @@
 		</ol>
 	</div>
 	<div class="row content">
-	<p>Please select the notifications you would like to recieve via e-mail.</p>
-	<form action="/dashboard/notifications" method="post">
-		<?php echo Form::select('notifications[]', $validNotifications, $selectedNotifications, array('multiple')); ?>
-		{{ Form::token() }}
-		<input type="submit" value="Save Settings">
-		
-	</form>
-	
+		<h1>Notifications</h1>
+		<p>Please select the notifications you would like to recieve via e-mail.</p>
+		<form action="/dashboard/notifications" method="post">
+			<div class="form-group">
+				<?php echo Form::select('notifications[]', $validNotifications, $selectedNotifications, array('multiple' => '', 'class' => 'form-control')); ?>
+				{{ Form::token() }}
+				<input type="submit" class="btn btn-default" value="Save Settings">
+			</div>
+		</form>
 	</div>
 @endsection
