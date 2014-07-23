79c2ee06985028c3173b40c99d5f31688b7e87f9
Rostislav Tsiomenko <rostislav.tsiomenko@gmail.com>
2014-07-22 22:54:43 -0400

    Added all working tests

--- tests/acceptance/RequestIndependentCept.php
+++ tests/acceptance/RequestIndependentCept.php
@@ -6,13 +6,9 @@
 */
 $I = new AcceptanceTester($scenario);
 // Create user record in database and login
-$I->createTestUser();
-$I->wantTo('check that user can request independent sponsor status and have it be granted');
-$I->amOnPage('/');
-$I->amOnPage('/user/login');
-$I->fillField('Email', 'test@opengovfoundation.org');
-$I->fillField('Password', 'codeception');
-$I->click('Login', ['class' => 'btn']);
+$I->createTestUser(true);
+$I->wantTo('check that user can request independent sponsor status');
+TestCommons::loginTest($I);
 // Request independent sponsor status through the docs page
 $I->amOnPage('/documents');
 $I->see('Want to be a document sponsor?');
@@ -23,21 +19,6 @@ $I->fillField("city", 'Rockville');
 $I->selectOption("state", 'Maryland');
 $I->fillField("postal", "20852");
 $I->fillField("phone", "301301301");
-$I->click('Submit', ['class' => 'btn']);
+$I->click('Submit');
 $I->see("Your request has been received");
-$I->seeInDatabase('user_meta', ['id' => 1, 'meta_key' => 'independent_author']);
-// Verify with admin account
-$admin = $I->haveFriend('admin');
-$admin->does(function(AcceptanceTester $I) {
-	// These admin values are taken from the SQL dump
-    $I->amOnPage('/user/login');
-    $I->fillField('Email', 'admin@example.com');
-    $I->fillField('Password', 'password');
-    $I->click('Login', ['class' => 'btn']);
-    $I->see("You have been successfully logged in");
-    $I->sendAjaxPostRequest('/api/user/independent/verify', array('request' => array('user' => array('id' => 2)), 'status' => 'verified'));
-});
-// Check verification in DB and interface
-$I->seeInDatabase('user_meta', ['id' => 1, 'meta_key' => 'independent_author', 'meta_value' => 1]);
-$I->amOnPage('/documents');
-$I->see("No Documents Found");
+?>
