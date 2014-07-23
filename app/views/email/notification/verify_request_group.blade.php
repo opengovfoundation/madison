13c16cc6b759a0932b3de1618de687a793648ead
Chris Birk <cmbirk@gmail.com>
2014-07-23 14:25:35 -0400

    finish group request verifications

--- app/views/email/notification/verify_request_group.blade.php
+++ app/views/email/notification/verify_request_group.blade.php
@@ -1,3 +1,4 @@
+{{-- Receives $group[] --}}
 <!DOCTYPE html>
 <html lang="en-US">
 	<head>
@@ -6,7 +7,15 @@
 	<body>
 
 		<p><strong>A group has requested verification:</strong></p>
-    {{ var_dump($group, true) }}
+
+    <p><strong>Name:</strong>  {{ $group['name'] }}</p>
+    <p><strong>Display Name:</strong> {{ $group['display_name'] }}</p>
+    <p><strong>Address 1:</strong> {{ $group['address1'] }}</p>
+    <p><strong>Address 2:</strong> {{ $group['address2'] }}</p>
+    <p><strong>City:</strong> {{ $group['city'] }}</p>
+    <p><strong>State:</strong> {{ $group['state'] }}</p>
+    <p><strong>Postal Code:</strong> {{ $group['postal_code'] }}</p>
+    <p><strong>Phone Number:</strong> {{ $group['phone_number'] }}</p>
 
 
 		<p>&ndash; The OpenGov Foundation Team</p>
