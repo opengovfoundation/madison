2b71554ddb041e08bcb026d24b028a446170e6da
Chris Birk <cmbirk@gmail.com>
2014-08-05 18:11:52 -0400

    set default for token in users table to empty string

---
+++ app/database/migrations/2014_08_05_220609_make_user_token_default_empty.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeUserTokenDefaultEmpty extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		DB::statement("ALTER TABLE users CHANGE token token varchar(25) NOT NULL DEFAULT ''");
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		DB::statement('ALTER TABLE users CHANGE token token varchar(25) NOT NULL');
+	}
+
+}
