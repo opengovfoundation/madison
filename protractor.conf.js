0e884a9a5350060915f1d51c5fd5002f158bead3
Rostislav Tsiomenko <rostislav.tsiomenko@gmail.com>
2014-08-21 17:45:24 -0400

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,12 +1,13 @@
 exports.config = {
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
-
+  debug: true.
   capabilities: {
     'browserName': 'chrome',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'Test triggered by Git push'
+    'name': 'Test triggered by Git push',
+    debug: true
   },
 
   // Spec patterns are relative to the current working directly when
