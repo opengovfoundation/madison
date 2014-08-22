8b6307d3052f377630b3ab18382945529d6e3b5e
Rostislav Tsiomenko <rostislav.tsiomenko@gmail.com>
2014-08-21 20:16:47 -0400

    Changed travis file

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
   // Spec patterns are relative to the current working directly when
   // protractor is called.
   specs: ['test/e2e/basic.spec.js'],
-  baseUrl: 'localhost:8000',
+  baseUrl: 'http://madison:8000',
   // Options to be passed to Jasmine-node.
   mochaOpts: { }
 };
