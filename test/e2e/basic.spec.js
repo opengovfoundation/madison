9227db8c8ecb94c9a86fd6f49ada463418cf6803
Rostislav Tsiomenko <rostislav.tsiomenko@gmail.com>
2014-08-25 10:25:32 -0400

    Changed E2E test

--- test/e2e/basic.spec.js
+++ test/e2e/basic.spec.js
@@ -1,11 +1,6 @@
 describe('Auth tests', function() {
 
-  // This is a sanity test to make sure Sauce is connecting
-  // If this fails first, it's easy to troubleshoot
-  it('should have a title', function() {
-    browser.get('/');
-    expect(browser.getTitle()).toEqual('Madison Home');
-  });
+
 
   // Check simple sign in
   it('should be able to sign in', function() {
