03fca308f6d8d9029abe3c219e113a6057c7d331
Rostislav Tsiomenko <rostislav.tsiomenko@gmail.com>
2014-08-26 13:39:40 -0400

    Fix protractor conf

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,7 +2,7 @@ exports.config = {
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
   capabilities: {
-    'browserName': 'chrome',
+    'browserName': 'firefox',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER,
     'name': 'Test triggered by Git push',
@@ -12,7 +12,7 @@ exports.config = {
   // Spec patterns are relative to the current working directly when
   // protractor is called.
   specs: ['test/e2e/*.spec.js'],
-  baseUrl: 'http://madison/',
+  baseUrl: 'http://madison:8000/',
   // Options to be passed to Jasmine-node.
   jasmineNodeOpts: {defaultTimeoutInterval: 100000 }
 };
