b8765ae128f068a19847e316d3e7b59db7450649
Chris Birk <cmbirk@gmail.com>
2014-09-18 15:44:16 -0400

    fix tab spacing in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,4 @@
+/*global window*/
 window.jQuery = window.$;
 
 var imports = [
@@ -23,23 +24,23 @@ var app = angular.module('madisonApp', imports);
 
 app.config(['growlProvider', '$httpProvider', '$routeProvider', function (growlProvider, $httpProvider, $routeProvider) {
     //Set up growl notifications
-    growlProvider.messagesKey("messages");
-    growlProvider.messageTextKey("text");
-    growlProvider.messageSeverityKey("severity");
-    $httpProvider.responseInterceptors.push(growlProvider.serverMessagesInterceptor);
-    growlProvider.onlyUniqueMessages(true);
-    growlProvider.globalTimeToLive(5000);
+  growlProvider.messagesKey("messages");
+  growlProvider.messageTextKey("text");
+  growlProvider.messageSeverityKey("severity");
+  $httpProvider.responseInterceptors.push(growlProvider.serverMessagesInterceptor);
+  growlProvider.onlyUniqueMessages(true);
+  growlProvider.globalTimeToLive(5000);
 
-    $routeProvider
-        .when('/user/edit/:user/notifications', {
-            templateUrl: "/templates/pages/user-notification-settings.html",
-            controller: "UserNotificationsController",
-            title: "Notification Settings"
-        });
+  $routeProvider
+    .when('/user/edit/:user/notifications', {
+      templateUrl: "/templates/pages/user-notification-settings.html",
+      controller: "UserNotificationsController",
+      title: "Notification Settings"
+    });
 }]);
 
 app.config(function ($locationProvider) {
-    $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(true);
 });
 
 window.console = window.console || {};
