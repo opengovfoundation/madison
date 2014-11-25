d99b3877ac8a85287b152d2a40c9cde4463cdcd5
Chris Birk <cmbirk@gmail.com>
2014-11-25 15:57:29 -0500

    trust proxy

--- app/filters.php
+++ app/filters.php
@@ -14,9 +14,7 @@ use Zizaco\Entrust\Entrust;
 
 App::before(function($request)
 {
-	if(! Request::secure()){
-		return Redirect::secure(Request::path());
-	}
+	Request::setTrustedProxies([$request->getClientIP()]);
 });
 
 
