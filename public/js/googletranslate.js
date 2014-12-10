21ea5a5ddc5b199e897a82a1b9445afaf01dcd5e
Chris Birk <cmbirk@gmail.com>
2014-12-10 15:57:39 -0500

    convert tabs to spaces for jslint

--- public/js/googletranslate.js
+++ public/js/googletranslate.js
@@ -1,3 +1,3 @@
 function googleTranslateElementInit() {
-  	new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
 }
