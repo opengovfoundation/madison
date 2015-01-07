7b5825166f54e8abb12cd31bc30d7a134cc822ed
Chris Birk <cmbirk@gmail.com>
2015-01-06 19:18:32 -0500

    separate angular filter files

---
+++ public/js/filters/gravatarFilter.js
@@ -0,0 +1,13 @@
+angular.module('madisonApp.filters')
+  .filter('gravatar', function () {
+    return function (email) {
+      var hash = '';
+
+      if (email !== undefined) {
+        hash = CryptoJS.MD5(email.toLowerCase());
+      }
+
+
+      return hash;
+    };
+  });
---
+++ public/js/filters/parseDateFilter.js
@@ -0,0 +1,6 @@
+angular.module('madisonApp.filters')
+  .filter('parseDate', function () {
+    return function (date) {
+      return Date.parse(date);
+    };
+  });
