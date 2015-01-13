e1576f9f4281737ca0f44048e770dac9e003828b
Salvador Mendo <simiographics@gmail.com>
2015-01-13 11:41:03 -0600

    Layout para settings, verificar cuenta

--- app/views/dashboard/settings.blade.php
+++ app/views/dashboard/settings.blade.php
@@ -1,32 +1,32 @@
 @extends('layouts/main')
 @section('content')
-	<div class="row">
-		<div class="col-md-3">
+	<div class="container">
+		<div class="row">
 			<ol class="breadcrumb">
 				<li><a href="/dashboard">{{ trans('messages.dashboard') }}</a></li>
 				<li class="active">{{ trans('messages.settings') }}</li>
 			</ol>
-		</div>
-	</div>
-	<div class="row">
-		<div class="content col-md-12">
-			<h1>Settings</h1>
-			<table class="table table-striped" ng-controller="DashboardSettingsController" ng-init="init()">
-				<thead>
-					<th>Contact</th>
-					<th>{{ trans('messages.fname') }}</th>
-					<th>{{ trans('messages.lname') }}</th>
-					<th>Email</th>
-				</thead>
-				<tbody>
-					<tr ng-repeat="admin in admins">
-						<td><input type="checkbox" ng-model="admin.admin_contact" ng-change="saveAdmin(admin)"><span class="glyphicon glyphicon-refresh" ng-show="admin.saved == false"></span></td>
-						<td>@{{ admin.fname }}</td>
-						<td>@{{ admin.lname }}</td>
-						<td>@{{ admin.email }}</td>
-					</tr>
-				</tbody>
-			</table>
+			<div class="col-md-12">
+				<div class="content">
+					<h1>Settings</h1>
+					<table class="table table-striped" ng-controller="DashboardSettingsController" ng-init="init()">
+						<thead>
+							<th>Contact</th>
+							<th>{{ trans('messages.fname') }}</th>
+							<th>{{ trans('messages.lname') }}</th>
+							<th>Email</th>
+						</thead>
+						<tbody>
+							<tr ng-repeat="admin in admins">
+								<td><input type="checkbox" ng-model="admin.admin_contact" ng-change="saveAdmin(admin)"><span class="glyphicon glyphicon-refresh" ng-show="admin.saved == false"></span></td>
+								<td>@{{ admin.fname }}</td>
+								<td>@{{ admin.lname }}</td>
+								<td>@{{ admin.email }}</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
 		</div>
 	</div>
 @endsection
