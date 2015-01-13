7777b948d02e4b9fb3114fa54f3632a1e1e1dc12
Salvador Mendo <simiographics@gmail.com>
2015-01-12 19:58:39 -0600

    Cambiar tipografia a Open Sans

--- app/views/page/home.blade.php
+++ app/views/page/home.blade.php
@@ -10,38 +10,44 @@
 		</div>
 	</div>
 	
-	<div class="container">
-		<div class="row" ng-controller="HomePageController" tourtip="@{{ step_messages.step_0 }}" tourtip-step="0">
-			<div class="col-sm-6">
-				<input tourtip="@{{ step_messages.step_1 }}" tourtip-step="1" id="doc-text-filter" type="text" ng-model="docSearch" class="form-control" placeholder="{{ trans('messages.filter') }}">
+	<div class="home-docs container">
+		<div ng-controller="HomePageController" tourtip="@{{ step_messages.step_0 }}" tourtip-step="0">
+
+			<div class="home-docs-filters row">
+				<div class="col-sm-6">
+					<input tourtip="@{{ step_messages.step_1 }}" tourtip-step="1" id="doc-text-filter" type="text" ng-model="docSearch" class="form-control" placeholder="{{ trans('messages.filter') }}">
+				</div>
+				<div class="col-sm-4 home-select2-container">
+					<select id="doc-category-filter" ui-select2="select2Config" ng-model="select2">
+						<option value=""></option>
+						<optgroup label="{{ trans('messages.category') }}">
+							<option value="category_@{{ category.id }}" ng-repeat="category in categories">@{{ category.name }}</option>
+						</optgroup>
+						<optgroup label="{{ trans('messages.sponsor') }}">
+							<option value="sponsor_@{{ sponsor.id }}" ng-repeat="sponsor in sponsors">@{{ sponsor.fname }} @{{ sponsor.lname }}</option>
+						</optgroup>
+						<optgroup label="{{ trans('messages.status') }}">
+							<option value="status_@{{ status.id}}" ng-repeat="status in statuses">@{{ status.label}}</option>
+						</optgroup>
+					</select>
+				</div>
+				<div class="col-sm-2 home-select2-container">
+					<select id="doc-date-filter" ui-select2="dateSortConfig" id="dateSortSelect" ng-model="dateSort">
+						<option value=""></option>
+						<option value="created_at">{{ trans('messages.posted') }}</option>
+						<option value="updated_at">{{ trans('messages.updated') }}</option>
+					</select>
+				</div>
 			</div>
-			<div class="col-sm-4 home-select2-container">
-				<select id="doc-category-filter" ui-select2="select2Config" ng-model="select2">
-					<option value=""></option>
-					<optgroup label="{{ trans('messages.category') }}">
-						<option value="category_@{{ category.id }}" ng-repeat="category in categories">@{{ category.name }}</option>
-					</optgroup>
-					<optgroup label="{{ trans('messages.sponsor') }}">
-						<option value="sponsor_@{{ sponsor.id }}" ng-repeat="sponsor in sponsors">@{{ sponsor.fname }} @{{ sponsor.lname }}</option>
-					</optgroup>
-					<optgroup label="{{ trans('messages.status') }}">
-						<option value="status_@{{ status.id}}" ng-repeat="status in statuses">@{{ status.label}}</option>
-					</optgroup>
-				</select>
-			</div>
-			<div class="col-sm-2 home-select2-container">
-				<select id="doc-date-filter" ui-select2="dateSortConfig" id="dateSortSelect" ng-model="dateSort">
-					<option value=""></option>
-					<option value="created_at">{{ trans('messages.posted') }}</option>
-					<option value="updated_at">{{ trans('messages.updated') }}</option>
-				</select>
-			</div>
-			<div class="col-sm-12">
-				<ul>
-					<li ng-repeat="doc in docs | toArray | filter:docSearch | orderBy:dateSort:reverse" ng-show="docFilter(doc)">
-						<div doc-list-item></div>
-					</li>
-				</ul>
+
+			<div class="home-docs-list row">
+				<div class="col-sm-12">
+					<ul>
+						<li ng-repeat="doc in docs | toArray | filter:docSearch | orderBy:dateSort:reverse" ng-show="docFilter(doc)">
+							<div doc-list-item></div>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
