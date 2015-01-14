8c51eb834d21c044347905c1b42b925d90daee14
Salvador Mendo <simiographics@gmail.com>
2015-01-14 15:45:04 -0600

    Add footer

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -1,7 +1,15 @@
-<div class="nav nav-footer col-md-12">
-	<a href="http://opengovfoundation.org" target="_blank">The OpenGov Foundation</a>
-	<a href="mailto:OpenGov Foundation <sayhello@opengovfoundation.org>?subject=Media Inquiry" target="_blank">Media Inquiries</a>
-	<a href="/terms-and-conditions">Terms &amp; Conditions</a>
-  <a href="/privacy-policy">Privacy Policy</a>
-  <a href="/copyright">Copyright Policy</a>
-</div>
+<footer class="main-footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4">
+        <img class="gobmx-footer" src="/svg/gob-mx-logo.svg" width="126" height="39" alt="gob.mx">
+      </div>
+      <div class="col-sm-4 text-center">
+        <img src="/svg/mover-mexico-logo.svg" width="172" height="70" alt="gob.mx">
+      </div>
+      <div class="col-sm-4 text-right">
+        <p>Insurgentes Sur 1735, Col. Guadalupe Inn. Delegación Álvaro Obregón México, D.F. C.P. 01020 </p>
+      </div>
+    </div>
+  </div>
+</footer>
--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -38,29 +38,11 @@
 			@include('errors')
 			@include('message')
 			@include('success')
-			
 			<div profile-completion-message></div>
-
-			<div id="wrap">
-				<div id="main" class="">
-					@yield('content')
-				</div>
-			</div>
-			<footer class="main-footer">
-			  <div class="container">
-			    <div class="row">
-			      <div class="col-sm-4">
-			        <img class="gobmx-footer" src="/svg/gob-mx-logo.svg" width="126" height="39" alt="gob.mx">
-			      </div>
-			      <div class="col-sm-4 text-center">
-			        <img src="/svg/mover-mexico-logo.svg" width="172" height="70" alt="gob.mx">
-			      </div>
-			      <div class="col-sm-4 text-right">
-			      	<p>Insurgentes Sur 1735, Col. Guadalupe Inn. Delegación Álvaro Obregón México, D.F. C.P. 01020 </p>
-			      </div>
-			    </div>
-			  </div>
-			</footer>
+			<main>
+				@yield('content')
+			</main>
+			@include('layouts.footer')
 		</tour>
 	</body>
 </html>
