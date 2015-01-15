e758324610f2314e23804bf567ab20d971be2c5e
Salvador Mendo <simiographics@gmail.com>
2015-01-14 21:24:57 -0600

    List, Edit and Members design fixes

--- app/views/groups/index.blade.php
+++ app/views/groups/index.blade.php
@@ -2,43 +2,41 @@
 @section('content')
 	<div class="container">
 		<div class="row">
-			<ol class="breadcrumb">
-				<li><a href="/dashboard">{{trans('messages.group')}}s</a></li>
-				<li class="active">{{ trans('messages.yourgroups') }}</li>
-			</ol>
 			<div class="col-md-12">
-				<div class="content">
-					<h1>{{ trans('messages.yourgroups') }}</h1>
-					<p>{{ trans('messages.wantcreategroup') }} <a href="/groups/edit">{{ trans('messages.clickhere') }}</a>.
-					@if(count($userGroups) <= 0)
-					<p>{{ trans('messages.notmembergroup') }}</p>
-					@else
-					<table class="table table-striped" id="groupsTable">
-						<thead>
-							<th>{{ trans('messages.displayname') }}</th>
-							<th>{{ trans('messages.groupname') }}</th>
-							<th>{{ trans('messages.yourrole') }}</th>
-							<th>{{ trans('messages.status') }}</th>
-						</thead>
-						<tbody>
-						<?php foreach($userGroups as $groupMember): ?>
-						<?php $group = $groupMember->group()->first(); ?>
-							<tr>
-								<?php if($group->isGroupOwner(Auth::user()->id)): ?>
-								<td><a href="/groups/edit/{{ $group->id }}">{{ $group->display_name ? $group->display_name : "N/A" }}</a></td>
-								<td><a href="/groups/edit/{{ $group->id }}">{{ $group->name }}</a></td>
-								<?php else: ?>
-								<td>{{ $group->display_name ? $group->display_name : "N/A" }}</td>
-								<td>{{ $group->name }}</td>
-								<?php endif; ?>
-								<td>{{ $group->findMemberByUserId(Auth::user()->id)->role }}</td>
-								<td>{{ $group->status }}</td>
-							</tr>
-						<?php endforeach; ?>
-						</tbody>
-					</table>
-					@endif
-				</div>
+				<ol class="breadcrumb">
+					<li><a href="/"><i class="icon icon-home"></i> {{ trans('messages.home')}}</a></li>
+					<li class="active">{{ trans('messages.yourgroups') }}</li>
+				</ol>
+				<h2>{{ trans('messages.yourgroups') }}</h2>
+				<p>{{ trans('messages.wantcreategroup') }} <a href="/groups/edit">{{ trans('messages.clickhere') }}</a>.
+				@if(count($userGroups) <= 0)
+				<p>{{ trans('messages.notmembergroup') }}</p>
+				@else
+				<table class="table table-striped" id="groupsTable">
+					<thead>
+						<th>{{ trans('messages.displayname') }}</th>
+						<th>{{ trans('messages.groupname') }}</th>
+						<th>{{ trans('messages.yourrole') }}</th>
+						<th>{{ trans('messages.status') }}</th>
+					</thead>
+					<tbody>
+					<?php foreach($userGroups as $groupMember): ?>
+					<?php $group = $groupMember->group()->first(); ?>
+						<tr>
+							<?php if($group->isGroupOwner(Auth::user()->id)): ?>
+							<td><a href="/groups/edit/{{ $group->id }}">{{ $group->display_name ? $group->display_name : "N/A" }}</a></td>
+							<td><a href="/groups/edit/{{ $group->id }}">{{ $group->name }}</a></td>
+							<?php else: ?>
+							<td>{{ $group->display_name ? $group->display_name : "N/A" }}</td>
+							<td>{{ $group->name }}</td>
+							<?php endif; ?>
+							<td>{{ $group->findMemberByUserId(Auth::user()->id)->role }}</td>
+							<td>{{ $group->status }}</td>
+						</tr>
+					<?php endforeach; ?>
+					</tbody>
+				</table>
+				@endif
 			</div>
 
 		</div>
--- app/views/groups/members/index.blade.php
+++ app/views/groups/members/index.blade.php
@@ -2,38 +2,36 @@
 @section('content')
 	<div class="container">
 		<div class="row">
-			<ol class="breadcrumb">
-				<li><a href="/dashboard">{{ trans('messages.group') }}s</a></li>
-				<li><a href="/groups/edit/{{ $group->id }}">{{ $group->display_name }}</a></li>
-				<li class="active">{{ trans('messages.groupmembers') }}</li>
-			</ol>
 			<div class="col-md-12">
-				<div class="content">
-					<h1>{{ trans('messages.membersof') }} '{{ $group->name }}'</h1>
-					<?php if($group->status == Group::STATUS_ACTIVE): ?>
-					<p><a href="/groups/invite/{{ $group->id }}">{{ trans('messages.addnewmember') }}</a></p>
-					<?php endif; ?>
-					<table class="table table-striped" id="groupsTable">
-						<thead>
-							<th>{{ trans('messages.fname') }}</th>
-							<th>{{ trans('messages.role') }}</th>
-							<th>{{ trans('messages.joined') }}</th>
-							<th>&nbsp;</th>
-						</thead>
-						<tbody>
-						<?php foreach($groupMembers as $member) : ?>
-							<tr>
-								<td>{{ $member->getUserName() }}</td>
-								<td>
-								{{ Form::select('role', Group::getRoles(true), $member->role, array('id' => 'memberRoleSelect', 'data-member-id' => $member->id)) }}
-								</td>
-								<td>{{ $member->created_at }} </td>
-								<td><a href="/groups/member/{{ $member->id }}/delete">{{ trans('messages.remove') }}</a></td>
-							</tr>
-						<?php endforeach; ?>
-						</tbody>
-					</table>
-				</div>
+				<ol class="breadcrumb">
+					<li><a href="/"><i class="icon icon-home"></i> {{ trans('messages.home')}}</a></li>
+					<li><a href="/groups/edit/{{ $group->id }}">{{ $group->display_name }}</a></li>
+					<li class="active">{{ trans('messages.groupmembers') }}</li>
+				</ol>
+				<h2>{{ trans('messages.membersof') }} '{{ $group->name }}'</h2>
+				<?php if($group->status == Group::STATUS_ACTIVE): ?>
+				<p><a href="/groups/invite/{{ $group->id }}">{{ trans('messages.addnewmember') }}</a></p>
+				<?php endif; ?>
+				<table class="table table-striped" id="groupsTable">
+					<thead>
+						<th>{{ trans('messages.fname') }}</th>
+						<th>{{ trans('messages.role') }}</th>
+						<th>{{ trans('messages.joined') }}</th>
+						<th>&nbsp;</th>
+					</thead>
+					<tbody>
+					<?php foreach($groupMembers as $member) : ?>
+						<tr>
+							<td>{{ $member->getUserName() }}</td>
+							<td>
+							{{ Form::select('role', Group::getRoles(true), $member->role, array('id' => 'memberRoleSelect', 'data-member-id' => $member->id)) }}
+							</td>
+							<td>{{ $member->created_at }} </td>
+							<td><a href="/groups/member/{{ $member->id }}/delete">{{ trans('messages.remove') }}</a></td>
+						</tr>
+					<?php endforeach; ?>
+					</tbody>
+				</table>
 			</div>
 		</div>
 	</div>
