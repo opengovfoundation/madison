2e0783b8e78f80550f4409566b57a1bafb64b42f
Omar <cone-7@hotmail.com>
2015-01-14 18:38:24 -0600

    Archivo de migracion para agregar campo visiblec

---
+++ app/database/migrations/2015_01_15_001938_visiblec.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Visiblec extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('comments', function(Blueprint $table)
+		{
+			$table->integer('visiblec');
+			//
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('comments', function(Blueprint $table)
+		{
+			$table->dropColumn('visiblec');
+			//
+		});
+	}
+
+}
