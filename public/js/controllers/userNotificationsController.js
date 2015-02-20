9f422c4e25b9db0b6163d21f5b9b92579704606e
Chris Birk <cmbirk@gmail.com>
2015-02-20 15:03:41 -0500

    remove references to userservice in usernotificationscontroller

--- public/js/controllers/userNotificationsController.js
+++ public/js/controllers/userNotificationsController.js
@@ -1,14 +1,14 @@
 angular.module('madisonApp.controllers')
-  .controller('UserNotificationsController', ['$scope', '$http', 'UserService', function ($scope, $http, UserService) {
-    //Wait for AppController controller to load user
-    UserService.exists.then(function () {
-      $http.get('/api/user/' + $scope.user.id + '/notifications')
-        .success(function (data) {
-          $scope.notifications = data;
-        }).error(function (data) {
-          console.error("Error loading notifications: %o", data);
-        });
-    });
+  .controller('UserNotificationsController', ['$scope', '$http', 'SessionService', function ($scope, $http, SessionService) {
+    //This may not be necessary.
+    //$scope.user = SessionService.user;
+
+    $http.get('/api/user/' + $scope.user.id + '/notifications')
+      .success(function (data) {
+        $scope.notifications = data;
+      }).error(function (data) {
+        console.error("Error loading notifications: %o", data);
+      });
 
     //Watch for notification changes
     $scope.$watch('notifications', function (newValue, oldValue) {
