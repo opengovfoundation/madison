dfda8ce863bf312d52b592abcd132207970adcf0
Bill Hunt <bill@krues8dr.com>
2015-07-03 15:35:42 -0400

    Adding new templates & directives.  #579

---
+++ public/js/directives/commentActionsDirective.js
@@ -0,0 +1,29 @@
+angular.module('madisonApp.directives')
+  .directive('commentActions', ['loginPopupService', 'SessionService', '$http',
+    function(loginPopupService, SessionService, $http) {
+    return {
+      restrict: 'E',
+      templateUrl: '/templates/partials/comment-actions.html',
+      scope: {
+        obj: '=object'
+      },
+      controller: function ($scope) {
+        $scope.user = SessionService.user;
+        $scope.addAction = function (activity, action, $event) {
+          console.log($scope.user, activity, action);
+          if ($scope.user && $scope.user.id !== '') {
+            $http.post('/api/docs/' + activity.doc_id + '/' + activity.label + 's/' + activity.id + '/' + action)
+              .success(function (data) {
+                activity.likes = data.likes;
+                activity.dislikes = data.dislikes;
+                activity.flags = data.flags;
+              }).error(function (data) {
+                console.error(data);
+              });
+          } else {
+            loginPopupService.showLoginForm($event);
+          }
+        };
+      }
+    };
+  }]);
