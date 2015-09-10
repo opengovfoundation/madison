6c7ec3f981eb883af91dd234ccca690699ca4071
Bill Hunt <bill@krues8dr.com>
2015-09-10 17:03:37 -0400

    Changing anchor link, cleaning up assets.

--- app/views/email/notification/verify_request_user.blade.php
+++ app/views/email/notification/verify_request_user.blade.php
@@ -7,7 +7,7 @@
 
 		<p>A user - {{ $user['fname'] }} {{ $user['lname'] }} ({{ $user['email'] }}) has requested profile verification.</p>
 
-        <p><a href="{{ URL::to('administrative-dashboard/verify-account') }}#user-{{ $request['id'] }}">View Request</a></p>
+        <p><a href="{{ URL::to('administrative-dashboard/verify-account') }}#request-{{ $request['id'] }}">View Request</a></p>
 
 		<p>&ndash; The OpenGov Foundation Team</p>
 	</body>
