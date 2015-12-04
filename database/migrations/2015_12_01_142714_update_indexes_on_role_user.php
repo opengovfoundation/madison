99a490f9507152b18b95db7ecdc3cbba25dc260c
Seth Etter <sethetter@gmail.com>
2015-12-04 11:05:33 -0600

    Remove lines dropping keys from entrust tables

--- database/migrations/2015_12_01_142714_update_indexes_on_role_user.php
+++ database/migrations/2015_12_01_142714_update_indexes_on_role_user.php
@@ -13,13 +13,8 @@ class UpdateIndexesOnRoleUser extends Migration
     public function up()
     {
         Schema::table('role_user', function ($table) {
-            // Can't drop because of 
-            $table->dropForeign('assigned_roles_user_id_foreign');
-            $table->dropForeign('assigned_roles_role_id_foreign');
-
             $table->unique('id');
             $table->dropPrimary('PRIMARY');
-            //$table->dropColumn('id');
 
             $table->foreign('user_id')->references('id')->on('users')
                 ->onUpdate('cascade')->onDelete('cascade');
--- database/migrations/2015_12_01_142733_update_indexes_on_permission_role.php
+++ database/migrations/2015_12_01_142733_update_indexes_on_permission_role.php
@@ -13,12 +13,8 @@ class UpdateIndexesOnPermissionRole extends Migration
     public function up()
     {
         Schema::table('permission_role', function ($table) {
-            $table->dropForeign('permission_role_permission_id_foreign');
-            $table->dropForeign('permission_role_role_id_foreign');
-
             $table->unique('id');
             $table->dropPrimary('PRIMARY');
-            //$table->dropColumn('id');
 
             $table->foreign('permission_id')->references('id')->on('permissions')
                 ->onUpdate('cascade')->onDelete('cascade');
