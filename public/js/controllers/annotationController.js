bd5636cbdc5068a32d2fc59e9aa25a3637bf6884
Seth Etter <sethetter@gmail.com>
2015-12-06 10:23:38 -0600

    Fix permalink and labels for comments/annotations
    Label is used for API route building, so the adjustments made to them to
    accommodate for permalink building caused errors. Now they are handled
    separately.
    
    Also removed some duplicate code from `annotationController` that is handled in
    `commentController` now and left a TODO about another method that could be
    shared between the two somehow.
    
    Close #683

--- public/js/controllers/annotationController.js
+++ public/js/controllers/annotationController.js
@@ -116,6 +116,7 @@ angular.module('madisonApp.controllers')
                 });
               }
 
+              annotation.permalinkBase = 'annotation';
               annotation.label = 'annotation';
               annotation.commentsCollapsed = collapsed;
               $scope.annotations.push(annotation);
@@ -141,7 +142,8 @@ angular.module('madisonApp.controllers')
               //Then count the unique users for the responses to each annotation.
               for(var commentIndex in annotation.comments) {
                 var comment = annotation.comments[commentIndex];
-                annotation.comments[commentIndex].label = 'annsubcomment';
+                annotation.comments[commentIndex].permalinkBase = 'annsubcomment';
+                annotation.comments[commentIndex].label = 'comment';
                 annotation.comments[commentIndex].doc_id = annotation.doc_id;
                 if(annotationGroup.users.indexOf(comment.user.id) < 0) {
                   annotationGroup.users.push(comment.user.id);
@@ -183,55 +185,6 @@ angular.module('madisonApp.controllers')
           });
       };
 
-
-      $scope.getDocComments = function (docId) {
-        $http({
-          method: 'GET',
-          url: '/api/docs/' + docId + '/comments'
-        })
-          .success(function (data) {
-            angular.forEach(data, function (comment) {
-              var collapsed = false;
-              if ($scope.subCommentId) {
-                angular.forEach(comment.comments, function (subcomment) {
-                  if (subcomment.id == $scope.subCommentId) {
-                    collapsed = false;
-                    subcomment.label = 'comment';
-                  }
-                });
-              }
-              comment.commentsCollapsed = collapsed;
-              comment.label = 'comment';
-              comment.link = 'comment_' + comment.id;
-              $scope.annotations.push(comment);
-            });
-          })
-          .error(function (data) {
-            console.error("Error loading comments: %o", data);
-          });
-      };
-
-      $scope.commentSubmit = function () {
-
-        var comment = angular.copy($scope.comment);
-        comment.user = $scope.user;
-        comment.doc = $scope.doc;
-
-        $http.post('/api/docs/' + comment.doc.id + '/comments', {
-          'comment': comment
-        })
-          .success(function (data) {
-            comment.label = 'comment';
-            comment.user = data.user;
-            comment.created = data.created_at;
-            $scope.stream.push(comment);
-            $scope.comment.text = '';
-          })
-          .error(function (data) {
-            console.error("Error posting comment: %o", data);
-          });
-      };
-
       $scope.activityOrder = function (activity) {
         // Leaving this function in case we want to implement a more complex
         // ordering algorithm in the future
@@ -251,20 +204,23 @@ angular.module('madisonApp.controllers')
         }
       };
 
+      // TODO: duplicate in `commentController`, move somewhere shared
       $scope.subcommentSubmit = function (activity, subcomment) {
         subcomment.user = $scope.user;
 
         $.post('/api/docs/' + $scope.doc.id + '/' + activity.label + 's/' + activity.id + '/comments', {
           'comment': subcomment
         })
-          .success(function (data) {
-            activity.comments.push(data);
-            subcomment.text = '';
-            subcomment.user = '';
-            $scope.$apply();
-          }).error(function (data) {
-            console.error(data);
-          });
+        .success(function (data) {
+          data.permalinkBase = 'annsubcomment';
+          data.label = 'comment';
+          activity.comments.push(data);
+          subcomment.text = '';
+          subcomment.user = '';
+          $scope.$apply();
+        }).error(function (data) {
+          console.error(data);
+        });
       };
 
       $scope.showAnnotations = function(annotationGroup) {
