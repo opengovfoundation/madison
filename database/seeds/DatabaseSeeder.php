d2a3d8b10616324ffc91e8e1ecaeb5de7aa6a690
Seth Etter <sethetter@gmail.com>
2015-12-09 15:12:40 -0600

    Add seed data for annotations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -56,6 +56,7 @@ class DatabaseSeeder extends Seeder
         $this->call('RbacSeeder');
         $this->call('GroupsTableSeeder');
         $this->call('DocumentsTableSeeder');
+        $this->call('AnnotationsTableSeeder');
 
         if (App::environment() === 'testing') {
             $this->call('TestSeeder');
