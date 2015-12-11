2a4d862b4077fba535e3e4d1dc386e623ce7d365
Seth Etter <sethetter@gmail.com>
2015-12-11 13:55:40 -0600

    Enable capabilities in protractor

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,11 +2,11 @@ exports.config = {
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
 
-  //capabilities: {
-  //  'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-  //  'build': process.env.TRAVIS_BUILD_NUMBER,
-  //  'name': "PHP " + process.env.TRAVIS_PHP_VERSION + "-" + process.env.TRAVIS_COMMIT_MSG
-  //},
+  capabilities: {
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': "PHP " + process.env.TRAVIS_PHP_VERSION + "-" + process.env.TRAVIS_COMMIT_MSG
+  },
 
   // Spec patterns are relative to the current working directly when
   // protractor is called.
