b114fd017266e4ab7cbb649a9c332fd8ae3b922a
Seth Etter <sethetter@gmail.com>
2016-01-05 17:45:17 -0600

    Scope issue

--- tests/client/e2e/pages/login.js
+++ tests/client/e2e/pages/login.js
@@ -11,7 +11,7 @@ var LoginPage = function() {
     browser.get('/user/login');
     emailField.sendKeys('user@example.com');
     passwordField.sendKeys('password');
-    clickLoginButton();
+    clickLoginButton(loginButton);
     browser.driver.wait(
       protractor.until.elementLocated(by.css('.account-dropdown')),
       5000
@@ -22,14 +22,14 @@ var LoginPage = function() {
     browser.get('/user/login');
     emailField.sendKeys('user@example.com');
     passwordField.sendKeys('wrongpassword');
-    clickLoginButton();
+    clickLoginButton(loginButton);
   };
 
   this.loginUnconfirmed = function() {
     browser.get('/user/login');
     emailField.sendKeys('user2@example.com');
     passwordField.sendKeys('password');
-    clickLoginButton();
+    clickLoginButton(loginButton);
   };
 
   this.logout = function() {
@@ -44,12 +44,12 @@ var LoginPage = function() {
 
 // Convenience function to make sure login button is clickable first
 // Seems to be something that is needed during Safari tests
-function clickLoginButton() {
+function clickLoginButton(btn) {
   browser.driver.wait(
     protractor.until.elementLocated(by.css('.login-button')),
     5000
   );
-  loginButton.click();
+  btn.click();
 }
 
 module.exports = LoginPage;
