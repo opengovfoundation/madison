c8502ee3a52f269fc53e2f2be60b129d5655dd20
Seth Etter <sethetter@gmail.com>
2016-01-06 14:37:34 -0600

    Add seeding for Doc Categories

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -56,6 +56,7 @@ class DatabaseSeeder extends Seeder
         $this->call('RbacSeeder');
         $this->call('GroupsTableSeeder');
         $this->call('DocumentsTableSeeder');
+        $this->call('CategoriesTableSeeder');
         $this->call('CommentsTableSeeder');
         $this->call('AnnotationsTableSeeder');
 
