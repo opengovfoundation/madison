2ca1a2b79fd21e4f97ff6c83f249dcf56a8369de
Seth Etter <sethetter@gmail.com>
2016-02-22 14:41:30 -0600

    Fix sponsor displaying on doc page

--- public/js/controllers/documentPageController.js
+++ public/js/controllers/documentPageController.js
@@ -18,11 +18,10 @@ angular.module('madisonApp.controllers')
       $scope.setSponsor = function () {
         try {
           //If the sponsor is a group
-          if ($scope.doc.group_sponsor.length > 0) {
-            $scope.doc.sponsor = $scope.doc.group_sponsor;
-          } else if ($scope.doc.user_sponsor.length > 0) { //Otherwise it's an individual
-            $scope.doc.sponsor = $scope.doc.user_sponsor;
-            $scope.doc.sponsor[0].display_name = $scope.doc.sponsor[0].fname + ' ' + $scope.doc.sponsor[0].lname;
+          if ($scope.doc.group_sponsors.length > 0) {
+            $scope.doc.sponsor = $scope.doc.group_sponsors;
+          } else if ($scope.doc.user_sponsors.length > 0) { //Otherwise it's an individual
+            $scope.doc.sponsor = $scope.doc.user_sponsors;
           } else { //This document has no sponsor!
             console.error("No sponsor found.");
           }
