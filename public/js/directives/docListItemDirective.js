a2f3aaae276af8c17962a0e8d35dc4c6ba81951e
Seth Etter <sethetter@gmail.com>
2016-02-23 10:30:10 -0600

    Tooltips on discussion closed icon
    Issue #874, also pulling in lost tooltip code for annotation help icon on
    document page. This was done for DC, but got lost in a merge somewhere.

--- public/js/directives/docListItemDirective.js
+++ public/js/directives/docListItemDirective.js
@@ -1,7 +1,12 @@
 angular.module('madisonApp.directives')
-  .directive('docListItem', function () {
+  .directive('docListItem', function ($timeout) {
     return {
       restrict: 'A',
-      templateUrl: '/templates/directives/doc-list-item.html'
+      templateUrl: '/templates/directives/doc-list-item.html',
+      link: function(scope, el, attrs) {
+        $timeout(function() {
+          $('.discussion-closed').tooltip({});
+        }, 1000);
+      }
     };
   });
