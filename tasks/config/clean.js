c7b4fc5e2afd1786395626b5e2f660233fc69620
Bill Hunt <bill@krues8dr.com>
2016-03-10 09:03:00 -0500

    Use blesscss on stylesheets to break into multiple files.  #896 #893

--- tasks/config/clean.js
+++ tasks/config/clean.js
@@ -1,6 +1,6 @@
 module.exports = function (grunt) {
   grunt.config.set('clean', {
-    build: ['public/build/*.{js,css}', 'public/index.html']
+    build: ['public/build/*.{js,css,map}', 'public/index.html']
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
