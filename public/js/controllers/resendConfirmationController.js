34cfba6f7744c66674ef5b8eeb1af01e61953deb
Seth Etter <sethetter@gmail.com>
2016-03-14 15:51:15 -0500

    Wrap sending confirm token in `$timeout` for IE9
    This prevents the controller from double posting the token and resulting in a
    false error from the server that the token is invalid.
    
    Issue #893

--- public/js/controllers/resendConfirmationController.js
+++ public/js/controllers/resendConfirmationController.js
@@ -1,22 +1,28 @@
 angular.module('madisonApp.controllers')
   .controller('ResendConfirmationController', ['$scope', '$http', '$state',
-    '$stateParams', '$translate', 'pageService', 'SITE',
+    '$stateParams', '$translate', 'pageService', 'SITE', '$timeout',
     function ($scope, $http, $state, $stateParams, $translate, pageService,
-      SITE) {
+      SITE, $timeout) {
       pageService.setTitle($translate.instant('content.confirmationresend.title',
         {title: SITE.name}));
 
-      //If we're following a link from the verification email
-      if ($stateParams.token) {
-        $http.post('/api/user/verify-email', {
-          token: $stateParams.token
-        }).success(function (response) {
-          $state.go('index');
-        }).error(function (response) {
-          $state.go('login');
-          console.error(response);
-        });
-      }
+      // We do this in a `$timeout` because in IE9, when going
+      // from regular URL to non-HTML5 version, the controller loads twice and
+      // causes the token to be submitted twice.
+      $timeout(function() {
+        //If we're following a link from the verification email
+        if ($stateParams.token) {
+          console.log('posting token');
+          $http.post('/api/user/verify-email', {
+            token: $stateParams.token
+          }).success(function (response) {
+            $state.go('index');
+          }).error(function (response) {
+            $state.go('login');
+            console.error(response);
+          });
+        }
+      }, 500);
 
       $scope.resendConfirmation = function () {
         $http.post('/api/verification/resend', {
