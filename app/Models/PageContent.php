11030b3a11070e0cfa6f33abc9d7e9bcfbd7a5d9
Seth Etter <sethetter@gmail.com>
2016-04-14 13:43:13 -0500

    Switch to using markdown for page content
    Issue #679

--- app/Models/PageContent.php
+++ app/Models/PageContent.php
@@ -5,8 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Page;
 
+use GrahamCampbell\Markdown\Facades\Markdown;
+
 class PageContent extends Model
 {
+    protected $table = 'page_contents';
+
     /**
      * Mass assignable attributes
      *
@@ -20,5 +24,13 @@ class PageContent extends Model
     public function page()
     {
         return $this->belongsTo(Page::class);
+    }
+
+    /**
+     * Return HTML version of page content
+     */
+    public function html()
+    {
+        return Markdown::convertToHtml($this->content);
     }
 }
