20be51d934b22348afd98b8c1c2cbd5dc46bae05
Seth Etter <sethetter@gmail.com>
2016-04-22 18:03:32 -0400

    Fix to pages show route definition.
    Issue #679

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -229,7 +229,7 @@ Route::put('api/groups/{groupId}/members/{memberId}', 'GroupController@putMember
 
 // Page Routes
 Route::get('api/pages/', 'PageController@index');
-Route::get('api/pages/{page}', 'PageController@index');
+Route::get('api/pages/{page}', 'PageController@show');
 Route::post('api/pages/', 'PageController@store');
 Route::put('api/pages/{page}', 'PageController@update');
 Route::delete('api/pages/{page}', 'PageController@destroy');
