5ef4608934f8cd60c3f7956f34dbf651106699fe
Seth Etter <sethetter@gmail.com>
2016-05-06 11:00:15 -0500

    Avoid using in place `sed` for travis
    Issue #943

--- client/scripts/cachebust/locales.sh
+++ client/scripts/cachebust/locales.sh
@@ -1,1 +1,5 @@
-dt=1462415480 && mv build/locales build/locales- && sed -i '' -e "s/\/locales\//\/locales-\//g" ./build/*.js
+dt=1462415480
+tmp=`mktemp /tmp/app.min.js.XXXXXXXXXX`
+mv build/locales build/locales-$dt
+sed -e "s/\/locales\//\/locales-$dt\//g" ./build/app.min.js > $tmp
+mv $tmp ./build/app.min.js
