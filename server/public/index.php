b8d311caf2a7eb527a656304ec5f37174e9a0830
Seth Etter <sethetter@gmail.com>
2016-05-10 16:14:35 -0500

    Remove debugging attempt
    Issue #943

--- server/public/index.php
+++ server/public/index.php
@@ -18,7 +18,6 @@
 | loading any of our classes later on. It feels nice to relax.
 |
 */
-file_put_contents('/var/log/_delete-me.log', "MADE IT TO THE SERVER\n", FILE_APPEND);
 
 require __DIR__.'/../bootstrap/autoload.php';
 
