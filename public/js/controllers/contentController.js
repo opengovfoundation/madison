8397e08f9b48914dd51b2447349962a2e1c7015f
Bill Hunt <bill@krues8dr.com>
2016-06-09 13:51:46 -0400

    Adding video to about page. #1003

--- public/js/controllers/contentController.js
+++ public/js/controllers/contentController.js
@@ -4,8 +4,8 @@
  */
 angular.module('madisonApp.controllers')
   .controller('ContentController', ['$scope', '$stateParams', '$translate',
-    'pageService', 'SITE',
-    function ($scope, $stateParams, $translate, pageService, SITE) {
+    '$sce', 'pageService', 'SITE',
+    function ($scope, $stateParams, $translate, $sce, pageService, SITE) {
       var page = $stateParams.page.replace(/-/g, '');
       $translate('content.' + page + '.title', {title: SITE.name}).then(function(translation) {
         pageService.setTitle(translation);
@@ -14,7 +14,7 @@ angular.module('madisonApp.controllers')
         $scope.header = translation;
       });
       $translate('content.' + page + '.body', {title: SITE.name}).then(function(translation) {
-        $scope.body = translation;
+        $scope.body = $sce.trustAsHtml(translation);
       });
     }
   ]);
