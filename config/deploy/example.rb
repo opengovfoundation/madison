c8b3bfe0b10dc35fea00114ce2de7228da766553
Seth Etter <sethetter@gmail.com>
2016-07-01 15:37:18 -0500

    Note about `deploy_to` folder for cap

--- config/deploy/example.rb
+++ config/deploy/example.rb
@@ -5,4 +5,6 @@ server '1.2.3.4', # Server IP address / hostname
   user: 'deploy', # Server user to deploy as
   roles: %w{app}
 
+# If you're using chef to provision, this should match up with the "id"
+# attribute in your `data_bages/sites` entry.
 set :deploy_to, '/var/www/vhosts/madison' # Folder site will deploy to
