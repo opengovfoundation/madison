76681f4d7edfa7c260bd2966aecee7bcb4d560ac
Tanner Doshier <git@doshitan.com>
2016-07-07 15:15:28 -0500

    Don't need to try to login to seed categories

--- server/database/seeds/CategoriesTableSeeder.php
+++ server/database/seeds/CategoriesTableSeeder.php
@@ -9,14 +9,6 @@ class CategoriesTableSeeder extends Seeder
 {
     public function run()
     {
-        $adminEmail = Config::get('madison.seeder.admin_email');
-        $adminPassword = Config::get('madison.seeder.admin_password');
-
-        // Login as admin to create categories
-        $credentials = ['email' => $adminEmail, 'password' => $adminPassword];
-        Auth::attempt($credentials);
-        $admin = Auth::user();
-
         $doc = Doc::find(1);
 
         $cat1 = factory(Category::class)->create();
