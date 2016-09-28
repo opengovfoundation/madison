b7a09808d6a95bc4996093a86ed82b4d1a53e095
Seth Etter <sethetter@gmail.com>
2016-09-28 08:36:43 -0500

    Prevent hashmark from moving files to root
    Closes T36

--- client/scripts/cachebust/js.sh
+++ client/scripts/cachebust/js.sh
@@ -1,5 +1,5 @@
-hashmark -c build -r -l 6 '*.js' '{dir}/{name}-{hash}{ext}' | sed -e 's/:"/:"/g' > ./build/build.json
-replaceinfiles -s 'build/index.html' -d '{dir}/{base}' < ./build/build.json
+hashmark -c build -r -l 6 '*.js' './{dir}/{name}-{hash}{ext}' | sed -e 's/:"/:"/g' > ./build/build.json
+replaceinfiles -s 'build/index.html' -d './{dir}/{base}' < ./build/build.json
 tmp=`mktemp /tmp/index.html.XXXXXXXXXXXXX`
 sed -e 's/\/css\//\/css\//g' build/index.html > $tmp
 mv $tmp ./build/index.html
