7ce04687661e28abb85feace55a3a1c6b2c171b3
Tanner Doshier <git@doshitan.com>
2016-12-06 16:10:49 -0600

    Use our custom user name attribute for toolbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,7 +56,7 @@
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->displayName }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
