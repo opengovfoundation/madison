9959fe3a26258b8e745a1f4df666031cc3f3b5d2
Seth Etter <sethetter@gmail.com>
2016-12-09 11:24:47 -0600

    Move some controllers back
    Summary: Move `Controller` / `HomeController` back
    
    Test Plan: Look at it.
    
    Reviewers: doshitan
    
    Reviewed By: doshitan
    
    Differential Revision: https://phabricator.opengovfoundation.org/D56

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
     }
 
     /**
